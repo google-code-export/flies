b92176d65a111bf2af174c59ed36e6c9849084a4
Asgeir Frimannsson <asgeirf@redhat.com>
2009-09-07 10:04:57 +1000

    use Seam 2.2.1-SNAPSHOT to take advantage of some of the testing improvements

---
+++ flies-war/src/main/java/org/fedorahosted/flies/core/dao/ProjectDAO.java
@@ -0,0 +1,26 @@
+package org.fedorahosted.flies.core.dao;
+
+import org.fedorahosted.flies.core.model.Project;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+
+@Name("projectDAO")
+@AutoCreate
+public class ProjectDAO {
+
+	@In
+	Session session;
+	
+	public Project getBySlug(String slug){
+		return (Project) session.createCriteria(Project.class)
+			.add( Restrictions.naturalId()
+		        .set("slug", slug)
+		    	)
+		    .setCacheable(true)
+		    .setComment("ProjectDAO.getBySlug")
+		    .uniqueResult();
+	}
+}
