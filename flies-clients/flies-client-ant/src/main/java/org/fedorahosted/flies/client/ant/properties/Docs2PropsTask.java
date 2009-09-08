fcfa9383aed42a7c2275f7bab43bf9d7f7be7225
James Ni <jni@redhat.com>
2009-09-08 17:14:53 +0800

    Add integration tests for retrieving Projects as XML and JSON

---
+++ flies-clients/flies-client-ant/src/main/java/org/fedorahosted/flies/client/ant/properties/Docs2PropsTask.java
@@ -0,0 +1,74 @@
+package org.fedorahosted.flies.client.ant.properties;
+
+import java.io.File;
+import java.net.URL;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.taskdefs.MatchingTask;
+import org.fedorahosted.flies.adapter.properties.PropWriter;
+import org.fedorahosted.flies.rest.dto.Document;
+import org.fedorahosted.flies.rest.dto.Documents;
+
+public class Docs2PropsTask extends MatchingTask {
+
+    private String apiKey;
+    private boolean debug;
+    private File dstDir;
+    private String src;
+    private String url;
+    
+    @Override
+    public void execute() throws BuildException {
+	try {
+	    Unmarshaller m = null;
+	    if (debug) {
+		JAXBContext jc = Context.newJAXBContext();
+		m = jc.createUnmarshaller();
+	    }
+	    
+	    URL srcURL = Utility.createURL(src, getProject());
+	    
+	    // TODO use rest api to fetch Documents
+	    Documents docs = (Documents) m.unmarshal(srcURL);
+	    
+	    for (Document doc : docs.getDocuments()) {
+		PropWriter.write(doc, dstDir);
+	    }
+	} catch (Exception e) {
+	    throw new BuildException(e);
+	}
+    }
+    
+    @Override
+    public void log(String msg) {
+        super.log(msg+"\n\n");
+    }
+    
+//    private void logVerbose(String msg) {
+//	super.log(msg, org.apache.tools.ant.Project.MSG_VERBOSE);
+//    }
+    
+    public void setApiKey(String apiKey) {
+	this.apiKey = apiKey;
+    }
+    
+    public void setDebug(boolean debug) {
+	this.debug = debug;
+    }
+    
+    public void setDstDir(File dstDir) {
+	this.dstDir = dstDir;
+    }
+
+    public void setSrc(String src) {
+	this.src = src;
+    }
+    
+    public void setUrl(String url) {
+	this.url = url;
+    }
+
+}
---
+++ flies-rest-api/src/main/java/org/fedorahosted/flies/rest/client/DocumentResource.java
@@ -0,0 +1,47 @@
+package org.fedorahosted.flies.rest.client;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.fedorahosted.flies.rest.MediaTypes;
+import org.fedorahosted.flies.rest.dto.Document;
+import org.fedorahosted.flies.rest.dto.DocumentRefs;
+import org.fedorahosted.flies.rest.dto.Documents;
+import org.jboss.resteasy.client.ClientResponse;
+
+public interface DocumentResource {
+
+	@GET
+	@Path("/d/{documentId}")
+	@Produces({ MediaTypes.APPLICATION_FLIES_DOCUMENT_XML, MediaType.APPLICATION_JSON })
+	public ClientResponse<Document> getDocument(@PathParam("documentId") String documentId,
+			@QueryParam("includeTargets") String includeTargets);
+
+	@POST
+	@Path("/d/{documentId}")
+	@Consumes({ MediaTypes.APPLICATION_FLIES_DOCUMENT_XML, MediaType.APPLICATION_JSON })
+	public Response updateDocument(@PathParam("documentId") String documentId,
+			Document document);
+	
+	@PUT
+	@Consumes({ MediaTypes.APPLICATION_FLIES_DOCUMENT_XML, MediaType.APPLICATION_JSON })
+	public Response addDocument(Document document);
+
+	@POST
+	@Path("/replace")
+	@Consumes({ MediaTypes.APPLICATION_FLIES_DOCUMENTS_XML, MediaType.APPLICATION_JSON })
+	public Response replace(Documents documents);
+
+	@GET
+	@Produces({ MediaTypes.APPLICATION_FLIES_DOCUMENTS_XML, MediaType.APPLICATION_JSON })
+	public ClientResponse<DocumentRefs> getDocuments();
+	
+}
