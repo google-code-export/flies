ba89f5710466c5d2e67c87247e854f70bd43700c
Sean Flanigan <sflaniga@redhat.com>
2009-09-14 15:15:36 +1000

    rearranged DocumentResource/DocumentService; changed error handling in ant tasks

---
+++ flies-rest-client/src/main/java/org/fedorahosted/flies/rest/FliesClient.java
@@ -0,0 +1,47 @@
+package org.fedorahosted.flies.rest;
+
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.fedorahosted.flies.rest.client.DocumentResource;
+import org.fedorahosted.flies.rest.client.ProjectIterationResource;
+import org.fedorahosted.flies.rest.client.ProjectResource;
+import org.jboss.resteasy.client.ProxyFactory;
+import org.jboss.resteasy.plugins.providers.RegisterBuiltin;
+import org.jboss.resteasy.spi.ResteasyProviderFactory;
+
+public class FliesClient {
+
+	private final URI baseUri;
+	private final String apiKey;
+	private final FliesClientRequestFactory clientRequestFactory;
+	
+	public FliesClient(String baseUrl, String username, String apiKey) throws URISyntaxException{
+		this.baseUri = new URI(baseUrl);
+		this.apiKey = apiKey;
+		clientRequestFactory = new FliesClientRequestFactory(username, apiKey);
+	}
+		
+	public ProjectResource getProjectResource(){
+		return clientRequestFactory.getProjectResource(baseUri);
+	}
+	
+	public ProjectIterationResource getProjectIterationResource(String projectSlug){
+		URI resolvedUri = ProxyFactory.createUri(baseUri.toString() + "/projects/p/" + projectSlug + "/iterations");
+		return clientRequestFactory.getProjectIterationResource(resolvedUri);
+	}
+	
+	public DocumentResource getDocumentResource(String projectSlug, String iterationSlug){
+		URI resolvedUri = ProxyFactory.createUri(baseUri.toString() + "/projects/p/" + projectSlug + "/iterations/i/" + iterationSlug + "/documents");
+		return clientRequestFactory.getDocumentResource(resolvedUri); 
+	}
+	
+	public URI getBaseUri() {
+		return baseUri;
+	}
+	
+	public String getApiKey() {
+		return apiKey;
+	}
+}
---
+++ flies-war/src/main/java/org/fedorahosted/flies/rest/service/DocumentService.java
@@ -0,0 +1,154 @@
+package org.fedorahosted.flies.rest.service;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.fedorahosted.flies.core.dao.ProjectDAO;
+import org.fedorahosted.flies.core.dao.ProjectIterationDAO;
+import org.fedorahosted.flies.core.model.HProjectIteration;
+import org.fedorahosted.flies.repository.model.HDocument;
+import org.fedorahosted.flies.repository.model.HResource;
+import org.fedorahosted.flies.rest.MediaTypes;
+import org.fedorahosted.flies.rest.dto.Document;
+import org.fedorahosted.flies.rest.dto.DocumentRef;
+import org.fedorahosted.flies.rest.dto.DocumentRefs;
+import org.fedorahosted.flies.rest.dto.Documents;
+import org.fedorahosted.flies.rest.dto.Resource;
+import org.hibernate.Session;
+import org.jboss.resteasy.spi.NotFoundException;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.security.Restrict;
+
+@Name("documentService")
+@Path("/projects/p/{projectSlug}/iterations/i/{iterationSlug}/documents")
+public class DocumentService {
+	
+	@PathParam("projectSlug")
+	private String projectSlug;
+	
+	@PathParam("iterationSlug")
+	private String iterationSlug;
+
+	@In
+	ProjectDAO projectDAO;
+	
+	@In
+	ProjectIterationDAO projectIterationDAO;
+	
+	@In
+	Session session;
+	
+	@GET
+	@Path("/d/{documentId}")
+	@Produces({ MediaTypes.APPLICATION_FLIES_DOCUMENT_XML, MediaType.APPLICATION_JSON })
+	public Document getDocument(
+			@PathParam("documentId") String documentId,
+			@QueryParam("includeTargets") String includeTargets) {
+		
+		return null;
+	}
+
+//	@POST
+//	@Path("/d/{documentId}")
+//	@Consumes({ MediaTypes.APPLICATION_FLIES_DOCUMENT_XML, MediaType.APPLICATION_JSON })
+//	@Restrict("#{identity.loggedIn}")
+//	public Response updateDocument(
+//			@PathParam("documentId") String documentId,
+//			Document document) {
+//		return null;
+//	}
+	
+	@PUT
+	@Path("/d/{documentId}")
+	@Consumes({ MediaTypes.APPLICATION_FLIES_DOCUMENT_XML, MediaType.APPLICATION_JSON })
+	@Restrict("#{identity.loggedIn}")
+	public Response addDocument(Document document) throws URISyntaxException {
+		
+		HProjectIteration hProjectIteration = projectIterationDAO.getBySlug(projectSlug, iterationSlug);
+		
+		if(hProjectIteration == null)
+			throw new NotFoundException("Project Iteration not found");
+		
+		HDocument hDoc = new HDocument(document);
+		hProjectIteration.getContainer().getDocuments().add(hDoc);
+		try{
+			session.flush();
+			for(Resource res : document.getResources()) {
+				HResource hRes = HDocument.create(res);
+				hRes.setDocument(hDoc);
+				hDoc.getResourceTree().add(hRes);
+				session.flush();
+			}
+			return Response.created( new URI("/d/"+hDoc.getDocId())).build();
+		}
+		catch(Exception e){
+			return Response.notAcceptable(null).build();
+		}
+		
+	}
+
+	@GET
+	@Produces({ MediaTypes.APPLICATION_FLIES_DOCUMENTREFS_XML, MediaType.APPLICATION_JSON })
+	public DocumentRefs getDocumentRefs() {
+		HProjectIteration hProjectIteration = projectIterationDAO.getBySlug(projectSlug, iterationSlug);
+		
+		if(hProjectIteration == null)
+			throw new NotFoundException("No such Project Iteration");
+		
+		DocumentRefs documents = new DocumentRefs();
+
+		for(HDocument doc : hProjectIteration.getContainer().getDocuments() ){
+			documents.getDocuments().add( 
+					new DocumentRef(
+							new Document(
+									doc.getDocId(),
+									doc.getName(),
+									doc.getPath(),
+									doc.getContentType(),
+									doc.getRevision(),
+									doc.getLocale()
+									)
+							)
+					);
+		}
+		
+		return documents;
+	}
+
+	@POST
+	@Consumes({ MediaTypes.APPLICATION_FLIES_DOCUMENTS_XML, MediaType.APPLICATION_JSON })
+	@Restrict("#{identity.loggedIn}")
+	public Response addDocuments(Documents documents) {
+	    // TODO
+	    return Response.ok().build();
+	}
+
+	@GET
+	@Path("/all")
+	@Produces({ MediaTypes.APPLICATION_FLIES_DOCUMENTS_XML, MediaType.APPLICATION_JSON })
+	public Documents getDocuments() {
+	    // TODO
+	    return new Documents();
+	}
+
+	@PUT
+	@Consumes({ MediaTypes.APPLICATION_FLIES_DOCUMENTS_XML, MediaType.APPLICATION_JSON })
+	@Restrict("#{identity.loggedIn}")
+	public Response replace(Documents documents) {
+	    // TODO
+	    return Response.ok().build();
+	}
+
+}
