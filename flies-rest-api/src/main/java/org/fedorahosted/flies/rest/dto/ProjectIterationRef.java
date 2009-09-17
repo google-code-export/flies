430448a24b3bc41e0e75d21761648e7d3ab64d31
Sean Flanigan <sflaniga@redhat.com>
2009-09-17 10:14:38 +1000

    Removed obsolete constructors from HDocument/HProjectContainer, trying to improve copy constructor for HTextFlow

---
+++ flies-rest-api/src/main/java/org/fedorahosted/flies/rest/dto/ProjectIterationRef.java
@@ -0,0 +1,36 @@
+package org.fedorahosted.flies.rest.dto;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+
+
+@XmlRootElement(name="project-iteration", namespace=Namespaces.DOCUMENT)
+@XmlType(name="projectIterationRefType", namespace=Namespaces.DOCUMENT)
+public class ProjectIterationRef extends AbstractProjectIteration{
+	
+	private ProjectIteration ref;
+	
+	// private enum ProjecType type; 
+	
+	private ProjectIterationRef(){
+		super();
+	}
+	
+	public ProjectIterationRef(ProjectIteration project) {
+		super(project);
+		this.ref = project;
+	}
+	
+	@XmlJavaTypeAdapter(value = UriAdapter.class)
+	@XmlAttribute
+	public ProjectIteration getRef() {
+		return ref;
+	}
+	
+	public void setRef(ProjectIteration ref) {
+		this.ref = ref;
+	}
+}
---
+++ flies-war/src/main/java/org/fedorahosted/flies/repository/util/TranslationStatistics.java
@@ -0,0 +1,84 @@
+package org.fedorahosted.flies.repository.util;
+
+import org.fedorahosted.flies.rest.dto.TextFlowTarget.ContentState;
+
+public class TranslationStatistics {
+
+	private long approved;
+	private long forReview;
+	private long fuzzyMatch;
+	private long statNew;
+
+	public TranslationStatistics() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void set(ContentState status, long count){
+		switch(status){
+		case Final:
+			approved = count;
+			break;
+		case ForReview:
+			forReview = count;
+			break;
+		case Leveraged:
+			fuzzyMatch = count;
+			break;
+		case New:
+			statNew = count;
+			break;
+		}
+	}
+	
+	public TranslationStatistics(Long approved, Long forReview, Long fuzzyMatch,
+			Long statNew) {
+		this.approved = approved;
+		this.forReview = forReview;
+		this.fuzzyMatch = fuzzyMatch;
+		this.statNew = statNew;
+	}
+
+	public long get(ContentState status) {
+		switch (status) {
+		case Final:
+			return getApproved();
+		case ForReview:
+			return getForReview();
+		case Leveraged:
+			return getFuzzyMatch();
+		case New:
+		default:
+			return getNew();
+		}
+	}
+
+	public long getTotal() {
+		return approved + forReview + fuzzyMatch + statNew;
+	}
+
+	public long getNotApproved() {
+		return forReview + fuzzyMatch + statNew;
+	}
+
+	public long getApproved() {
+		return approved;
+	}
+
+	public long getForReview() {
+		return forReview;
+	}
+
+	public long getFuzzyMatch() {
+		return fuzzyMatch;
+	}
+
+	public long getNew() {
+		return statNew;
+	}
+
+	@Override
+	public String toString() {
+		return "{a:" + approved + ",r:" + forReview + ",f:" + fuzzyMatch
+				+ ",n:" + statNew + "}";
+	}
+}
