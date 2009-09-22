9e5d2848382dfe4dc67c81ff7cf5331d79da0b3b
Sean Flanigan <sflaniga@redhat.com>
2009-09-22 19:05:40 +1000

    Add test for deletion of omitted documents in PUT, implemented explicit deletes in put()

---
+++ flies-war/src/main/java/org/fedorahosted/flies/rest/service/DocumentsServiceActionImpl.java
@@ -0,0 +1,128 @@
+package org.fedorahosted.flies.rest.service;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.fedorahosted.flies.core.dao.DocumentDAO;
+import org.fedorahosted.flies.core.dao.ProjectContainerDAO;
+import org.fedorahosted.flies.repository.model.HDocument;
+import org.fedorahosted.flies.repository.model.HProjectContainer;
+import org.fedorahosted.flies.rest.dto.Document;
+import org.fedorahosted.flies.rest.dto.Documents;
+import org.hibernate.Session;
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Logger;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.log.Log;
+
+@AutoCreate
+@Scope(ScopeType.STATELESS)
+@Name("DocumentsServiceActionImpl")
+public class DocumentsServiceActionImpl implements DocumentsServiceAction {
+
+    @Logger 
+    private Log log;
+    
+    // To access properties projectSlug, iterationSlug and request
+    @In 
+    private DocumentsService documentsService; 
+    
+//    @In 
+//    private DocumentDAO documentDAO;
+    @In 
+    private ProjectContainerDAO projectContainerDAO;
+    @In 
+    private DocumentConverter documentConverter;
+    @In 
+    private Session session;
+	
+	private HProjectContainer getContainer() {
+		HProjectContainer result = projectContainerDAO.getBySlug(
+				documentsService.getProjectSlug(), 
+				documentsService.getIterationSlug());
+		if (result == null) {
+			throw new WebApplicationException(Response.status(Status.NOT_FOUND).entity("Container not found").build());
+		}
+		return result;
+	}
+    
+    public Documents get() {
+    	log.debug("HTTP GET {0}", documentsService.getRequest().getRequestURL());
+    	Collection<HDocument> hdocs = getContainer().getDocuments().values();
+    	Documents result = new Documents();
+	
+    	for (HDocument hDocument : hdocs) {
+    		result.getDocuments().add(hDocument.toDocument(true));
+    	}
+    	log.info("HTTP GET result :\n"+result);
+    	return result;
+    }
+    
+    public void post(Documents docs) {
+    	log.debug("HTTP POST {0} : \n{1}",documentsService.getRequest().getRequestURL(), docs);
+    	HProjectContainer hContainer = getContainer();
+    	Map<String, HDocument> docMap = hContainer.getDocuments();
+    	for (Document doc: docs.getDocuments()) {
+			// if doc already exists, load it and update it, but don't create it
+    		HDocument hDoc = docMap.get(doc.getId());
+    		if (hDoc == null) {
+    			log.info("POST creating new HDocument with id {0}", doc.getId());
+    			hDoc = new HDocument(doc);
+    			hDoc.setRevision(1);
+    			hDoc.setProject(hContainer);
+    		} else {
+    			log.info("POST updating HDocument with id {0}", doc.getId());
+    		}
+    		
+			docMap.put(hDoc.getDocId(), hDoc);
+    		session.save(hDoc);
+    		documentConverter.copy(doc, hDoc, true);
+    	}
+    	session.flush();
+    }
+    
+    
+    public void put(Documents docs) {
+    	log.debug("HTTP PUT {0} : \n{1}",documentsService.getRequest().getRequestURL(), docs);
+    	HProjectContainer hContainer = getContainer();
+//    	Map<String, HDocument> docMap = new HashMap<String, HDocument>(); 
+//    	hContainer.setDocuments(docMap);
+    	Map<String, HDocument> docMap = hContainer.getDocuments();
+    	Map<String, HDocument> oldMap = new HashMap<String, HDocument>(docMap); 
+    	docMap.clear();
+
+    	for (Document doc: docs.getDocuments()) {
+			// if doc already exists, load it and update it, but don't create it
+//    		HDocument hDoc = oldMap.get(doc.getId());
+    		HDocument hDoc = oldMap.remove(doc.getId());
+    		if (hDoc == null) {
+    			log.debug("PUT creating new HDocument with id {0}", doc.getId());
+    			hDoc = new HDocument(doc);
+    			hDoc.setRevision(1);
+    			hDoc.setProject(hContainer);
+    		} else {
+    			log.debug("PUT updating HDocument with id {0}", doc.getId());
+    		}
+    		docMap.put(hDoc.getDocId(), hDoc);
+    		session.save(hDoc);
+    		documentConverter.copy(doc, hDoc, true);
+    	}
+    	
+    	// why aren't implicit deletes working?
+    	// ensure omitted docs get deleted by Hibernate
+    	for (HDocument hDoc : oldMap.values()) {
+			session.delete(hDoc);
+		}
+    	session.flush();
+    	log.debug("final state :\n{0}", docs);
+    }
+
+}
---
+++ flies-war/src/test/java/org/fedorahosted/flies/rest/service/DocumentsServiceSeamTest.java
@@ -0,0 +1,163 @@
+package org.fedorahosted.flies.rest.service;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
+import java.net.URI;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
+import javax.ws.rs.core.Response;
+
+import org.dbunit.operation.DatabaseOperation;
+import org.fedorahosted.flies.ContentType;
+import org.fedorahosted.flies.LocaleId;
+import org.fedorahosted.flies.rest.ApiKeyHeaderDecorator;
+import org.fedorahosted.flies.rest.client.IDocumentsResource;
+import org.fedorahosted.flies.rest.dto.Document;
+import org.fedorahosted.flies.rest.dto.Documents;
+import org.fedorahosted.flies.rest.dto.Resource;
+import org.fedorahosted.flies.rest.dto.TextFlow;
+import org.fedorahosted.flies.rest.dto.TextFlowTarget;
+import org.jboss.resteasy.client.ClientRequestFactory;
+import org.jboss.resteasy.client.ClientResponse;
+import org.jboss.resteasy.plugins.providers.RegisterBuiltin;
+import org.jboss.resteasy.spi.ResteasyProviderFactory;
+import org.jboss.seam.mock.DBUnitSeamTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+@Test(groups = { "seam-tests" })
+public class DocumentsServiceSeamTest extends DBUnitSeamTest {
+
+	ClientRequestFactory clientRequestFactory;
+	IDocumentsResource docsService;
+
+	@BeforeClass
+	public void prepareRestEasyClientFramework() throws Exception {
+
+		ResteasyProviderFactory instance = ResteasyProviderFactory
+				.getInstance();
+		RegisterBuiltin.register(instance);
+
+		clientRequestFactory = new ClientRequestFactory(
+				new SeamMockClientExecutor(this), (URI)null);
+
+		clientRequestFactory.getPrefixInterceptors().registerInterceptor(
+				new ApiKeyHeaderDecorator("admin",
+						"12345678901234567890123456789012"));
+
+		docsService = clientRequestFactory
+				.createProxy(IDocumentsResource.class, 
+					"/restv1/projects/p/sample-project/iterations/i/1.1/documents");
+
+	}
+
+	@Override
+	protected void prepareDBUnitOperations() {
+	    beforeTestOperations.add(new DataSetOperation(
+		    "org/fedorahosted/flies/test/model/DocumentsData.dbunit.xml",
+		    DatabaseOperation.CLEAN_INSERT));
+	    afterTestOperations.add(new DataSetOperation(
+			    "org/fedorahosted/flies/test/model/DocumentsData.dbunit.xml",
+			    DatabaseOperation.DELETE_ALL));
+	}
+
+	public void getZero() throws Exception {
+		expectDocs();
+	}
+	
+	private void expectDocs(Document... docs) {
+	    ClientResponse<Documents> response = docsService.getDocuments();
+	    
+	    assertThat(response.getStatus(), is(200));
+	    assertThat(response.getEntity(), notNullValue());
+	    Set<String> expected = new TreeSet<String>();
+	    for (Document doc : docs) {
+			expected.add(doc.toString());
+		}
+//	    assertThat(response.getEntity().getDocuments().size(), is(Arrays.asList(docs).size()));
+	    Set<String> actual = new TreeSet<String>();
+	    for (Document doc : response.getEntity().getDocuments()) {
+	    	// leave links out of the XML
+	    	doc.getLinks().clear();
+			actual.add(doc.toString());
+		}
+	    assertThat(actual, is(expected));
+	}
+	
+	private Document newDoc(String id, Resource... resources) {
+		ContentType contentType = ContentType.TextPlain;
+		Integer version = 1;
+		Document doc = new Document(id, id+"name", id+"path", contentType, version, LocaleId.EN);
+		for (Resource textFlow : resources) {
+			doc.getResources(true).add(textFlow);
+		}
+		return doc;
+	}
+	
+	private TextFlow newTextFlow(String id, String sourceContent, String targetLocale, String targetContent) {
+		TextFlow textFlow = new TextFlow(id, LocaleId.EN);
+	    textFlow.setContent(sourceContent);
+	    TextFlowTarget target = new TextFlowTarget(textFlow, LocaleId.fromJavaName(targetLocale));
+	    target.setContent(targetContent);
+		textFlow.addTarget(target);
+		return textFlow;
+	}
+
+	private Document putDoc1() {
+		Documents docs = new Documents();
+		Document doc = newDoc("foo.properties", 
+				newTextFlow("FOOD", "Slime Mould", "de_DE", "Sauerkraut"));
+		docs.getDocuments().add(doc);
+		Response response = docsService.put(docs);
+		assertThat(response.getStatus(), is(200));
+		return doc;
+	}
+	
+	private Document postDoc2() {
+	    Documents docs = new Documents();
+	    Document doc = newDoc("test.properties",
+	    		newTextFlow("HELLO", "Hello World", "fr", "Bonjour le Monde"));
+		docs.getDocuments().add(doc);
+	    Response response = docsService.post(docs);
+	    assertThat(response.getStatus(), is(200));
+		return doc;
+	}
+	
+	public void putGet() throws Exception {
+	    getZero();
+	    Document doc1 = putDoc1();
+	    expectDocs(doc1);
+	}
+
+
+	
+	public void putPostGet() throws Exception {
+	    getZero();
+	    Document doc1 = putDoc1();
+	    expectDocs(doc1);
+	    Document doc2 = postDoc2();
+	    expectDocs(doc1, doc2);
+	}
+	
+	
+	public void put2Then1() throws Exception {
+	    getZero();
+	    Document doc1 = putDoc1();
+	    Document doc2 = postDoc2();
+	    expectDocs(doc1, doc2);
+	    // this put should have the effect of deleting doc2
+		putDoc1();
+	    expectDocs(doc1);
+	}
+	
+	// TODO expect 404 for non-existent project
+//	public void getBadProject() {
+//		
+//	}
+
+}
