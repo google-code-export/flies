335b3e2b056ec1f7fa4e429168fe263290959255
Asgeir Frimannsson <asgeirf@redhat.com>
2009-09-22 13:02:09 +1000

    consolidate XML namespaces and create tool to generate XML schema

---
+++ flies-clients/flies-client-ant/src/main/java/org/fedorahosted/flies/client/ant/properties/Docs2PropsTask.java
@@ -0,0 +1,90 @@
+package org.fedorahosted.flies.client.ant.properties;
+
+import java.io.File;
+import java.net.URL;
+import java.util.Set;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.taskdefs.MatchingTask;
+import org.fedorahosted.flies.adapter.properties.PropWriter;
+import org.fedorahosted.flies.rest.FliesClientRequestFactory;
+import org.fedorahosted.flies.rest.client.IDocumentsResource;
+import org.fedorahosted.flies.rest.dto.Document;
+import org.fedorahosted.flies.rest.dto.Documents;
+import org.jboss.resteasy.client.ClientResponse;
+
+public class Docs2PropsTask extends MatchingTask {
+
+    private String user;
+    private String apiKey;
+    private boolean debug;
+    private File dstDir;
+    private String src;
+    
+    @Override
+    public void execute() throws BuildException {
+	try {
+	    Unmarshaller m = null;
+	    if (debug) {
+		JAXBContext jc = JAXBContext.newInstance(Documents.class);
+		m = jc.createUnmarshaller();
+	    }
+	    
+	    URL srcURL = Utility.createURL(src, getProject());
+
+	    Set<Document> docList;
+	    if("file".equals(srcURL.getProtocol())) {
+		Documents docs = (Documents) m.unmarshal(new File(srcURL.getFile()));
+		docList = docs.getDocuments();
+	    } else {
+		// use rest api to fetch Documents
+		FliesClientRequestFactory factory = new FliesClientRequestFactory(user, apiKey);
+		IDocumentsResource documentsResource = factory.getDocumentsResource(srcURL.toURI());
+		ClientResponse<Documents> response  = documentsResource.getDocuments();
+		
+		Utility.checkResult(response, srcURL);
+		docList = response.getEntity().getDocuments();
+	    }
+	    
+	    for (Document doc : docList) {
+		PropWriter.write(doc, dstDir);
+	    }
+	} catch (Exception e) {
+	    throw new BuildException(e);
+	}
+    }
+    
+    @Override
+    public void log(String msg) {
+        super.log(msg+"\n\n");
+    }
+    
+//    private void logVerbose(String msg) {
+//	super.log(msg, org.apache.tools.ant.Project.MSG_VERBOSE);
+//    }
+    
+    
+    public void setApiKey(String apiKey) {
+	this.apiKey = apiKey;
+    }
+    
+    public void setDebug(boolean debug) {
+	this.debug = debug;
+    }
+    
+    public void setDstDir(File dstDir) {
+	this.dstDir = dstDir;
+    }
+
+    public void setSrc(String src) {
+	this.src = src;
+    }
+    
+    public void setUser(String user) {
+	this.user = user;
+    }
+    
+}
---
+++ flies-rest-api/src/main/java/org/fedorahosted/flies/rest/dto/Documents.java
@@ -0,0 +1,35 @@
+package org.fedorahosted.flies.rest.dto;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+import org.jboss.resteasy.annotations.providers.jaxb.json.Mapped;
+import org.jboss.resteasy.annotations.providers.jaxb.json.XmlNsMap;
+
+@Mapped(namespaceMap = {
+	@XmlNsMap(namespace = Namespaces.FLIES, jsonName = Namespaces.FLIES_JSON), 
+	@XmlNsMap(namespace = Namespaces.XML, jsonName = Namespaces.XML_JSON) 
+})
+@XmlRootElement(name="documents", namespace=Namespaces.FLIES)
+@XmlType(name="documentsType", namespace=Namespaces.FLIES )
+public class Documents {
+	
+	private Set<Document> documents;
+	
+	@XmlElement(name="document", namespace=Namespaces.FLIES)
+	public Set<Document> getDocuments() {
+		if(documents == null)
+			documents = new HashSet<Document>();
+		return documents;
+	}
+	
+	@Override
+	public String toString() {
+		return Utility.toXML(this);
+	}
+	
+}
