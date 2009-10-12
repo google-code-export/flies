06c0d87fafb4067e61c35bf2395f8ac5cad0345b
Asgeir Frimannsson <asgeirf@redhat.com>
2009-10-13 09:43:49 +1000

    having some issues with the older gwt-maven plugin - bumping version

---
+++ flies-webtrans/src/main/java/org/fedorahosted/flies/webtrans/editor/HasPageCount.java
@@ -0,0 +1,6 @@
+package org.fedorahosted.flies.webtrans.editor;
+
+public interface HasPageCount {
+	public void setPageCount(int pageCount);
+	public int getPageCount();
+}
---
+++ flies-webtrans/src/main/java/org/fedorahosted/flies/webtrans/editor/TransUnitColumnDefinition.java
@@ -0,0 +1,10 @@
+package org.fedorahosted.flies.webtrans.editor;
+
+
+import org.fedorahosted.flies.webtrans.model.TransUnit;
+
+import com.google.gwt.gen2.table.client.AbstractColumnDefinition;
+
+public abstract class TransUnitColumnDefinition<ColType> extends AbstractColumnDefinition<TransUnit, ColType>{
+	
+}
