ea09b392b0a9e067497202be068bb95b3c862deb
Sean Flanigan <sflaniga@redhat.com>
2009-10-13 10:43:08 +1000

    Created module flies-client-pseudo

---
+++ flies-webtrans/src/main/java/org/fedorahosted/flies/webtrans/client/ui/HeadingPanel.java
@@ -0,0 +1,60 @@
+package org.fedorahosted.flies.webtrans.client.ui;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import com.google.gwt.user.client.ui.DecoratorPanel;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.Widget;
+
+public class HeadingPanel extends HorizontalPanel {
+
+	private Widget heading;
+
+	public HeadingPanel() {
+		addStyleName("gwt-HeadingPanel");
+	}
+	
+	public HeadingPanel(Widget heading) {
+		this();
+		setHeadingWidget(heading);
+	}
+	
+	public void setHeadingWidget(Widget heading) {
+		this.heading = heading;
+		//getCellElement(0, 1).appendChild(heading.getElement());
+		//adopt(this.heading);
+	}
+
+	public Iterator<Widget> iterator() {
+		final Iterator<Widget> superIterator = super.iterator();
+		return new Iterator<Widget>() {
+			boolean hasTitle = heading != null;
+
+			public boolean hasNext() {
+				return superIterator.hasNext() || hasTitle;
+			}
+
+			public Widget next() {
+				if (superIterator.hasNext()) {
+					return superIterator.next();
+				} else if (hasTitle) {
+					hasTitle = false;
+					return heading;
+				} else {
+					throw new NoSuchElementException();
+				}
+			}
+
+			public void remove() {
+				throw new UnsupportedOperationException();
+			}
+		};
+	}
+	
+	public Widget getWidget() {
+		return this;
+	}
+}
