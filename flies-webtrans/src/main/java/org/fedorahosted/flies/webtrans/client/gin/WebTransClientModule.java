107a463802e287274b57ec307b380ab5d9256e33
James Ni <jni@redhat.com>
2009-10-13 14:11:26 +0800

    Add a Progress bar to Web Translation UI and implement the TransUnitInfoPresenter and view.

---
+++ flies-webtrans/src/main/java/org/fedorahosted/flies/webtrans/client/gin/WebTransClientModule.java
@@ -0,0 +1,54 @@
+package org.fedorahosted.flies.webtrans.client.gin;
+
+import net.customware.gwt.presenter.client.DefaultEventBus;
+import net.customware.gwt.presenter.client.EventBus;
+import net.customware.gwt.presenter.client.gin.AbstractPresenterModule;
+import net.customware.gwt.presenter.client.place.PlaceManager;
+
+import org.fedorahosted.flies.webtrans.client.AppPresenter;
+import org.fedorahosted.flies.webtrans.client.DocumentListPresenter;
+import org.fedorahosted.flies.webtrans.client.DocumentListView;
+import org.fedorahosted.flies.webtrans.client.TransUnitInfoPresenter;
+import org.fedorahosted.flies.webtrans.client.TransUnitInfoView;
+import org.fedorahosted.flies.webtrans.client.WestNavigationPresenter;
+import org.fedorahosted.flies.webtrans.client.WestNavigationView;
+import org.fedorahosted.flies.webtrans.client.WorkspaceUsersPresenter;
+import org.fedorahosted.flies.webtrans.client.WorkspaceUsersView;
+import org.fedorahosted.flies.webtrans.client.ui.Pager;
+import org.fedorahosted.flies.webtrans.editor.CachedWebTransTableModel;
+import org.fedorahosted.flies.webtrans.editor.HasPageNavigation;
+import org.fedorahosted.flies.webtrans.editor.TransUnitListPresenter;
+import org.fedorahosted.flies.webtrans.editor.WebTransScrollTable;
+import org.fedorahosted.flies.webtrans.editor.TransUnitTableDefinition;
+import org.fedorahosted.flies.webtrans.editor.WebTransTableModel;
+
+import com.google.gwt.user.client.ui.HasValue;
+import com.google.inject.Singleton;
+
+public class WebTransClientModule extends AbstractPresenterModule {
+
+	/**
+	 * The Binding EDSL is described in {@link com.google.inject.Binder}
+	 */
+	@Override
+	protected void configure() {		
+		bind(EventBus.class).to(DefaultEventBus.class).in(Singleton.class);
+		bind(PlaceManager.class).in(Singleton.class);
+		
+		bind(WebTransTableModel.class).in(Singleton.class);
+		bind(CachedWebTransTableModel.class).in(Singleton.class);
+		bind(TransUnitTableDefinition.class).in(Singleton.class);
+		bind(WebTransScrollTable.class).in(Singleton.class);
+		
+		bindPresenter(DocumentListPresenter.class, DocumentListPresenter.Display.class, DocumentListView.class);
+		bindPresenter(TransUnitInfoPresenter.class, TransUnitInfoPresenter.Display.class, TransUnitInfoView.class);
+		bindPresenter(TransUnitListPresenter.class, TransUnitListPresenter.Display.class, WebTransScrollTable.class);
+		bindPresenter(WestNavigationPresenter.class, WestNavigationPresenter.Display.class, WestNavigationView.class);
+		bindPresenter(WorkspaceUsersPresenter.class, WorkspaceUsersPresenter.Display.class, WorkspaceUsersView.class);
+		bind(HasPageNavigation.class).to(WebTransScrollTable.class).in(Singleton.class);
+		bind(AppPresenter.class).in(Singleton.class);
+		//bind(DispatchAsync.class);
+		
+	}
+
+}
