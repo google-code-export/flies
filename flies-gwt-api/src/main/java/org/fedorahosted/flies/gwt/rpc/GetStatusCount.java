e06257eaa528122e11471d2052fc2cab288ed8df
Caius Chance <cchance at redhat.com@localhost>
2009-10-23 17:18:51 +1000

    Added user name to popup panel. Adjusted popup position.

---
+++ flies-gwt-api/src/main/java/org/fedorahosted/flies/gwt/rpc/GetStatusCount.java
@@ -0,0 +1,33 @@
+package org.fedorahosted.flies.gwt.rpc;
+
+import net.customware.gwt.dispatch.shared.Action;
+
+import org.fedorahosted.flies.gwt.model.DocumentId;
+import org.fedorahosted.flies.gwt.model.LocaleId;
+import org.fedorahosted.flies.gwt.model.TransUnitId;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+public class GetStatusCount implements Action<GetStatusCountResult>, IsSerializable{
+
+	private static final long serialVersionUID = -1218943735746130251L;
+
+	private DocumentId documentId;
+
+	@SuppressWarnings("unused")
+	public GetStatusCount(){
+	}
+	
+	public GetStatusCount(DocumentId id) {
+		this.documentId = id;
+	}
+
+	public DocumentId getDocumentId() {
+		return documentId;
+	}
+	
+	public void setDocumentId(DocumentId documentId) {
+		this.documentId = documentId;
+	}
+
+}
---
+++ flies-webtrans/src/main/java/org/fedorahosted/flies/webtrans/editor/WebTransScrollTable.java
@@ -0,0 +1,132 @@
+package org.fedorahosted.flies.webtrans.editor;
+
+
+import org.fedorahosted.flies.gwt.model.TransUnit;
+
+import com.allen_sauer.gwt.log.client.Log;
+import com.google.gwt.event.logical.shared.HasSelectionHandlers;
+import com.google.gwt.event.logical.shared.SelectionEvent;
+import com.google.gwt.event.logical.shared.SelectionHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.gen2.table.client.FixedWidthGridBulkRenderer;
+import com.google.gwt.gen2.table.client.PagingScrollTable;
+import com.google.gwt.gen2.table.client.ScrollTable;
+import com.google.gwt.gen2.table.client.SelectionGrid.SelectionPolicy;
+import com.google.gwt.gen2.table.event.client.HasPageChangeHandlers;
+import com.google.gwt.gen2.table.event.client.HasPageCountChangeHandlers;
+import com.google.gwt.gen2.table.event.client.RowSelectionEvent;
+import com.google.gwt.gen2.table.event.client.RowSelectionHandler;
+import com.google.gwt.gen2.table.event.client.TableEvent.Row;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HasWidgets;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.inject.Inject;
+import com.weborient.codemirror.client.ParserSyntax;
+import com.weborient.codemirror.client.SyntaxToggleWidget;
+
+public class WebTransScrollTable extends PagingScrollTable<TransUnit> implements
+		TransUnitListPresenter.Display, HasSelectionHandlers<TransUnit>, HasPageNavigation{
+
+	private final CachedWebTransTableModel cachedTableModel;
+	
+	@Inject
+	public WebTransScrollTable(CachedWebTransTableModel tableModel, TransUnitTableDefinition tableDefinition) {
+		super(tableModel,tableDefinition);
+		this.cachedTableModel = tableModel;
+		Log.info("setting up TransUnitListView");
+		setupScrollTable();
+	}
+	
+	@Override
+	public Widget asWidget() {
+		return this;
+	}
+
+	@Override
+	public void startProcessing() {
+	}
+
+	@Override
+	public void stopProcessing() {
+	}
+
+	private SyntaxToggleWidget syntaxWidget;
+
+	protected void setupScrollTable() {
+		setSize("100%", "100%");
+
+		syntaxWidget = new SyntaxToggleWidget(ParserSyntax.MIXED, true);
+		
+		// Create the scroll table
+		setPageSize(50);
+		
+		setEmptyTableWidget(new HTML(
+				"There is no data to display"));
+
+		// Setup the bulk renderer
+		FixedWidthGridBulkRenderer<TransUnit> bulkRenderer = new FixedWidthGridBulkRenderer<TransUnit>(
+				getDataTable(), this);
+		setBulkRenderer(bulkRenderer);
+
+		// Setup the formatting
+		setCellPadding(3);
+		setCellSpacing(0);
+		setResizePolicy(ScrollTable.ResizePolicy.FILL_WIDTH);
+		
+		setSize("100%", "100%");
+
+		getDataTable().setSelectionPolicy(SelectionPolicy.ONE_ROW);
+		getDataTable().setCellPadding(3);
+		
+		getDataTable().addRowSelectionHandler(new RowSelectionHandler() {
+			@Override
+			public void onRowSelection(RowSelectionEvent event) {
+				if(!event.getSelectedRows().isEmpty()){
+					Row row = event.getSelectedRows().iterator().next();
+					TransUnit tu = getRowValue(row.getRowIndex());
+					SelectionEvent.fire(WebTransScrollTable.this, tu);
+				}
+			}
+		});
+		
+	}
+	
+	private FlowPanel toolbar;
+	
+	@Override
+	public HandlerRegistration addSelectionHandler(
+			SelectionHandler<TransUnit> handler) {
+		return addHandler(handler, SelectionEvent.getType());
+	}
+
+	@Override
+	public HasSelectionHandlers<TransUnit> getSelectionHandlers() {
+		return this;
+	}
+	
+	@Override
+	public HasWidgets getToolbar() {
+		return toolbar;
+	}
+
+	@Override
+	public HasPageNavigation getPageNavigation() {
+		return this;
+	}
+	
+	@Override
+	public HasPageChangeHandlers getPageChangeHandlers() {
+		return this;
+	}
+
+	@Override
+	public HasPageCountChangeHandlers getPageCountChangeHandlers() {
+		return this;
+	}
+	
+	public CachedWebTransTableModel getCachedTableModel() {
+		return cachedTableModel;
+	}
+
+}
