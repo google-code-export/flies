5f8c6409ea6e594218711ebdc6dd1c41878f68f0
Sean Flanigan <sflaniga@redhat.com>
2009-10-23 16:19:09 +1000

    Changed DTOs and Hibernate model objects to use 'revision', not 'version'

---
+++ flies-war/src/main/java/org/fedorahosted/flies/rest/service/DocumentService.java
@@ -0,0 +1,193 @@
+package org.fedorahosted.flies.rest.service;
+
+import java.net.URISyntaxException;
+import java.util.Set;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.core.Response.Status;
+
+import org.fedorahosted.flies.LocaleId;
+import org.fedorahosted.flies.core.dao.DocumentDAO;
+import org.fedorahosted.flies.core.dao.ProjectContainerDAO;
+import org.fedorahosted.flies.repository.model.HDocument;
+import org.fedorahosted.flies.repository.model.HProjectContainer;
+import org.fedorahosted.flies.rest.MediaTypes;
+import org.fedorahosted.flies.rest.client.ContentQualifier;
+import org.fedorahosted.flies.rest.dto.Container;
+import org.fedorahosted.flies.rest.dto.Document;
+import org.fedorahosted.flies.rest.dto.Resource;
+import org.fedorahosted.flies.rest.dto.ResourceList;
+import org.hibernate.Session;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.security.Restrict;
+
+@Name("documentService")
+@Path("/projects/p/{projectSlug}/iterations/i/{iterationSlug}/documents/d/{documentId}")
+public class DocumentService {
+	
+	@PathParam("projectSlug")
+	private String projectSlug;
+	
+	@PathParam("iterationSlug")
+	private String iterationSlug;
+
+	@PathParam("documentId")
+	private String documentId;
+	
+    @In 
+    private DocumentConverter documentConverter;
+
+    @In
+	DocumentDAO documentDAO;
+	
+	@In
+	ProjectContainerDAO projectContainerDAO;
+	
+	@In
+	Session session;
+	
+	@Context
+	UriInfo uri;
+	
+	@GET
+	@Produces({ MediaTypes.APPLICATION_FLIES_DOCUMENT_XML, MediaTypes.APPLICATION_FLIES_DOCUMENT_JSON, 
+				MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public Response get(
+			@QueryParam("resources") @DefaultValue("") ContentQualifier resources) {
+		
+		HProjectContainer hProjectContainer = getContainerOrFail();
+		String docId = URIHelper.convertFromDocumentURIId(documentId);
+		
+		HDocument hDoc = documentDAO.getByDocId(hProjectContainer, docId);
+		
+		if(hDoc == null) {
+			return Response.status(Status.NOT_FOUND).entity("Document not found").build();
+		}
+		
+		int requestedLevels = resources.isNone() ? 0 : Integer.MAX_VALUE;
+		Set<LocaleId> requestedLanguages = resources.getLanguages();
+		if (resources.isAll()) {
+			requestedLanguages = documentDAO.getTargetLocales(hDoc); 
+		}
+		
+		Document doc = hDoc.toDocument(requestedLevels); 
+		documentConverter.addLinks(doc,uri.getRequestUri(), 
+				uri.getBaseUri().resolve(URIHelper.getIteration(projectSlug, iterationSlug)));
+		
+		return Response.ok().entity(doc).tag("v-" + doc.getRevision()).build();
+	}
+	
+	@PUT
+	@Consumes({ MediaTypes.APPLICATION_FLIES_DOCUMENT_XML, MediaTypes.APPLICATION_FLIES_DOCUMENT_JSON,
+				MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@Restrict("#{identity.loggedIn}")
+	public Response put(Document document) throws URISyntaxException {
+		
+		String hDocId = URIHelper.convertFromDocumentURIId(documentId);
+
+		if(!document.getId().equals(hDocId)){
+			return Response.status(Status.BAD_REQUEST).entity("Invalid document Id").build();
+		}
+
+		HProjectContainer hProjectContainer = getContainerOrFail();
+
+		HDocument hDoc = documentDAO.getByDocId(hProjectContainer, hDocId);
+		
+		if(hDoc == null) { // it's a create operation
+			hDoc = documentConverter.create(document, hProjectContainer);
+			hProjectContainer.getDocuments().put(hDoc.getDocId(), hDoc);
+			session.save(hDoc);
+			try{
+				session.flush();
+				return Response.created( uri.getBaseUri().resolve(URIHelper.getDocument(projectSlug, iterationSlug, documentId))).build();
+			}
+			catch(Exception e){
+				// TODO validation on the input data
+				// this could also be a server error
+				return Response.status(Status.BAD_REQUEST).entity("Invalid document content").build();
+			}
+		}
+		else{ // it's an update operation
+			if(!hDoc.getRevision().equals(document.getRevision())) {
+				return Response.status(Status.CONFLICT).entity("Version conflict").build();
+			}
+			documentConverter.merge(document, hDoc);
+			session.flush();
+			return Response.status(205).build();
+		}
+		
+	}
+	
+	@GET
+	@Path("content/{qualifier}")
+	public Response getContent(
+			@PathParam("qualifier") ContentQualifier qualifier,
+			@QueryParam("levels") @DefaultValue("1") int levels){
+		ResourceList resources = new ResourceList();
+		return Response.ok().entity(resources).build();
+	}
+
+	@POST
+	@Path("content/{qualifier}")
+	@Restrict("#{identity.loggedIn}")
+	public Response postContent(
+			ResourceList content,
+			@PathParam("qualifier") ContentQualifier qualifier) {
+		
+		return Response.ok().build();
+	}
+	
+	@PUT
+	@Path("content/{qualifier}")
+	public Response putContent(
+			ResourceList content,
+			@PathParam("qualifier") ContentQualifier qualifier) {
+		
+		return Response.ok().build();
+	}
+
+	@POST
+	@Path("content/{qualifier}/{resourceId}")
+	@Restrict("#{identity.loggedIn}")
+	public Response postContentByResourceId(
+			Resource resource,
+			@PathParam("qualifier") ContentQualifier qualifier,
+			@PathParam("resourceId") String resourceId) {
+		
+		return Response.ok().build();
+	}
+	
+	@GET
+	@Path("content/{qualifier}/{resourceId}")
+	public Response getContentByResourceId(
+			@PathParam("qualifier") ContentQualifier qualifier,
+			@PathParam("resourceId") String resourceId,
+			@QueryParam("levels") @DefaultValue("1") int levels){
+		return Response.ok().entity(new Container("id")).build();
+	}
+	
+	private HProjectContainer getContainerOrFail(){
+		HProjectContainer hProjectContainer = projectContainerDAO.getBySlug(projectSlug, iterationSlug); 
+		
+		if(hProjectContainer == null)
+			throw new WebApplicationException(
+					Response.status(Status.NOT_FOUND).entity("Project Container not found").build());
+		
+		return hProjectContainer;
+	}
+
+}
---
+++ flies-webtrans/src/main/java/org/fedorahosted/flies/webtrans/client/LoginPresenter.java
@@ -0,0 +1,122 @@
+package org.fedorahosted.flies.webtrans.client;
+
+import org.fedorahosted.flies.gwt.rpc.AuthenticateAction;
+import org.fedorahosted.flies.gwt.rpc.AuthenticateResult;
+import org.fedorahosted.flies.webtrans.client.NotificationEvent.Severity;
+import org.fedorahosted.flies.webtrans.client.auth.Identity;
+import org.fedorahosted.flies.webtrans.client.auth.LoginResult;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.event.dom.client.HasKeyUpHandlers;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.client.Cookies;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.PasswordTextBox;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.inject.Inject;
+
+import net.customware.gwt.dispatch.client.DispatchAsync;
+import net.customware.gwt.presenter.client.EventBus;
+import net.customware.gwt.presenter.client.place.Place;
+import net.customware.gwt.presenter.client.place.PlaceRequest;
+import net.customware.gwt.presenter.client.widget.WidgetDisplay;
+import net.customware.gwt.presenter.client.widget.WidgetPresenter;
+
+public class LoginPresenter extends WidgetPresenter<LoginPresenter.Display> {
+
+	public static final Place PLACE = new Place("LoginPanel");
+	
+	public interface Display extends WidgetDisplay {
+		Button getLoginButton();
+		Button getCancelButton();
+		TextBox getUsernameField();
+		PasswordTextBox getPasswordField();
+		void show();
+		void hide();
+		void showError();
+	}
+
+	private final Identity identity;
+	
+	@Inject
+	public LoginPresenter(Identity identity, Display display, EventBus eventBus) {
+		super(display, eventBus);
+		this.identity = identity;
+	}
+
+	@Override
+	public Place getPlace() {
+		return PLACE;
+	}
+
+	@Override
+	protected void onBind() {
+		
+		display.getLoginButton().addClickHandler(clickHandler);
+		display.getCancelButton().addClickHandler(clickHandler);
+		display.getUsernameField().addKeyUpHandler(keyHandler);
+		display.getPasswordField().addKeyUpHandler(keyHandler);
+	}
+
+	@Override
+	protected void onPlaceRequest(PlaceRequest request) {
+	}
+
+	private final ClickHandler clickHandler = new ClickHandler() {
+		@Override
+		public void onClick(ClickEvent event) {
+			if(event.getSource() == display.getLoginButton()) {
+				tryLogin();
+			}
+			else{
+				display.hide();
+			}
+		}
+	};
+	
+	private final KeyUpHandler keyHandler = new KeyUpHandler() {
+		
+		@Override
+		public void onKeyUp(KeyUpEvent event) {
+			if( event.getNativeKeyCode() == KeyCodes.KEY_ENTER ) {
+				tryLogin();
+			}
+		}
+	};
+
+	private void tryLogin() {
+		display.startProcessing();
+		identity.login(display.getUsernameField().getText(), display.getPasswordField().getText(), new LoginResult() {
+			@Override
+			public void onSuccess() {
+				display.stopProcessing();
+				display.hide();
+			}
+			
+			@Override
+			public void onFailure() {
+				display.stopProcessing();
+				display.showError();
+			}
+		});
+	}
+	
+	@Override
+	protected void onUnbind() {
+	}
+
+	@Override
+	public void refreshDisplay() {
+	}
+
+	@Override
+	public void revealDisplay() {
+		display.show();
+	}
+	
+}
