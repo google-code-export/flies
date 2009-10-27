c375454c4e10d711c41dfe769938faf9b901a9e8
Sean Flanigan <sflaniga@redhat.com>
2009-10-27 16:04:30 +1000

    Changed JBoss launch profiles for Eclipse to use a string substitution variable to find JBoss home

---
+++ flies-war/src/main/java/org/fedorahosted/flies/webtrans/gwt/GwtActionDispatcher.java
@@ -0,0 +1,41 @@
+package org.fedorahosted.flies.webtrans.gwt;
+
+import net.customware.gwt.dispatch.server.DefaultDispatch;
+import net.customware.gwt.dispatch.shared.Action;
+import net.customware.gwt.dispatch.shared.ActionException;
+import net.customware.gwt.dispatch.shared.Result;
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.Create;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+
+@Name("gwtActionDispatcher")
+@Scope(ScopeType.APPLICATION)
+@AutoCreate
+public class GwtActionDispatcher {
+
+	private SeamActionHandlerRegistry actionHandlerRegistry;
+
+	@Create
+	public void init() {
+		actionHandlerRegistry = new SeamActionHandlerRegistry();
+		addHandlers();
+	}
+
+	private void addHandlers() {
+		actionHandlerRegistry.addHandler( new GetTransUnitsHandler());
+		actionHandlerRegistry.addHandler( new GetDocsListHandler());
+		actionHandlerRegistry.addHandler(new GetWorkspaceContextHandler());
+		actionHandlerRegistry.addHandler(new UpdateTransUnitHandler());
+		actionHandlerRegistry.addHandler(new AuthenticateHandler());
+		actionHandlerRegistry.addHandler(new GetStatusCountHandler());
+	}
+
+	public Result execute(final Action<? extends Result> action)
+			throws ActionException {
+		final DefaultDispatch dd = new DefaultDispatch(actionHandlerRegistry);
+
+		return dd.execute(action);
+	}
+}
