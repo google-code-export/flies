c1a737d6f10d146bc7931bb3a8dfcf1ac67e3df6
Asgeir Frimannsson <asgeirf@redhat.com>
2009-10-28 11:36:55 +1000

    rethrow seam-based authorization/authentication exceptions to clients

---
+++ flies-gwt-api/src/main/java/org/fedorahosted/flies/gwt/rpc/GetTransUnits.java
@@ -0,0 +1,61 @@
+package org.fedorahosted.flies.gwt.rpc;
+
+import org.fedorahosted.flies.gwt.model.DocumentId;
+import org.fedorahosted.flies.gwt.model.LocaleId;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+import net.customware.gwt.dispatch.shared.Action;
+
+public class GetTransUnits implements Action<GetTransUnitsResult>, IsSerializable {
+
+	private static final long serialVersionUID = -6560254574690557950L;
+
+	private int offset;
+	private int count;
+	private DocumentId documentId;
+	private LocaleId localeId;
+
+	@SuppressWarnings("unused")
+	private GetTransUnits(){
+	}
+	
+	public GetTransUnits(DocumentId id, LocaleId localeId, int offset, int count) {
+		this.documentId = id;
+		this.localeId = localeId;
+		this.offset = offset;
+		this.count = count;
+	}
+
+	public int getOffset() {
+		return offset;
+	}
+
+	public void setOffset(int offset) {
+		this.offset = offset;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public DocumentId getDocumentId() {
+		return documentId;
+	}
+	
+	public void setDocumentId(DocumentId documentId) {
+		this.documentId = documentId;
+	}
+	
+	public LocaleId getLocaleId() {
+		return localeId;
+	}
+	
+	public void setLocaleId(LocaleId localeId) {
+		this.localeId = localeId;
+	}
+}
