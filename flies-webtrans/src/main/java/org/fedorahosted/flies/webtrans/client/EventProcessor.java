497de2a90182fa0949d2adbb369222d2b72ca3fa
Sean Flanigan <sflaniga@redhat.com>
2009-11-04 10:16:43 +1000

    Automated merge with ssh://seanf@hg.fedorahosted.org//hg/flies

--- flies-webtrans/src/main/java/org/fedorahosted/flies/webtrans/client/EventProcessor.java
+++ flies-webtrans/src/main/java/org/fedorahosted/flies/webtrans/client/EventProcessor.java
@@ -1,15 +1,17 @@
 package org.fedorahosted.flies.webtrans.client;
 
-import java.util.Iterator;
-
-import org.fedorahosted.flies.gwt.rpc.GetEventsAction;
-import org.fedorahosted.flies.gwt.rpc.GetEventsResult;
-import org.fedorahosted.flies.gwt.rpc.SessionEvent;
-import org.fedorahosted.flies.gwt.rpc.TransUnitUpdated;
-import org.fedorahosted.flies.webtrans.client.events.TransUnitUpdatedEvent;
+import java.util.HashMap;
 
 import net.customware.gwt.dispatch.client.DispatchAsync;
 import net.customware.gwt.presenter.client.EventBus;
+
+import org.fedorahosted.flies.gwt.rpc.GetEventsAction;
+import org.fedorahosted.flies.gwt.rpc.GetEventsResult;
+import org.fedorahosted.flies.gwt.rpc.HasTransUnitUpdatedData;
+import org.fedorahosted.flies.gwt.rpc.SessionEvent;
+import org.fedorahosted.flies.gwt.rpc.SessionEventData;
+import org.fedorahosted.flies.gwt.rpc.TransUnitUpdated;
+import org.fedorahosted.flies.webtrans.client.events.TransUnitUpdatedEvent;
 
 import com.allen_sauer.gwt.log.client.Log;
 import com.google.gwt.event.shared.GwtEvent;
@@ -23,15 +25,49 @@ public class EventProcessor extends Timer{
 	private final DispatchAsync dispatcher;
 	private final WorkspaceContext workspaceContext;
 	
+	private interface EventFactory<T extends GwtEvent<?>> {
+		T create(SessionEvent<?> event);
+	}
+	
+	private static class EventRegistry{
+		HashMap<Class<? extends SessionEventData>, EventFactory<?>> factories = 
+			new HashMap<Class<? extends SessionEventData>, EventFactory<?>>();
+
+		public EventRegistry() {
+			
+			// put any additional factories here
+			factories.put(TransUnitUpdated.class, new EventFactory<TransUnitUpdatedEvent>() {
+				@Override
+				public TransUnitUpdatedEvent create(SessionEvent<?> event) {
+					return new TransUnitUpdatedEvent(
+							(HasTransUnitUpdatedData) event.getData(), event.getSequence());
+				}
+			});
+		}
+		
+		public GwtEvent<?> getEvent(SessionEvent<?> sessionEvent) {
+			EventFactory<?> factory = factories.get(sessionEvent.getData().getClass());
+			if(factories == null) {
+				Log.warn("Could not find factory for class " +
+						sessionEvent.getData().getClass());
+				return null;
+			}
+			return factory.create(sessionEvent);
+		}
+	}
+	
+	private final EventRegistry eventRegistry;
+	
 	@Inject
 	public EventProcessor(EventBus eventBus, DispatchAsync dispatcher, WorkspaceContext workspaceContext) {
 		this.eventBus = eventBus;
 		this.dispatcher = dispatcher;
 		this.workspaceContext = workspaceContext;
+		this.eventRegistry = new EventRegistry();
 		scheduleRepeating(3000);
 	}
 	
-	private int lastOffset = 0;
+	private int lastSequence = 0;
 	private boolean running = false;
 	@Override
 	public void run() {
@@ -39,26 +75,20 @@ public class EventProcessor extends Timer{
 			Log.info("Running timer");
 			running = true;
 			dispatcher.execute( new GetEventsAction(
-					workspaceContext.getProjectContainerId().getId(), workspaceContext.getLocaleId(), lastOffset), 
+					workspaceContext.getProjectContainerId().getId(), workspaceContext.getLocaleId(), lastSequence), 
 						new AsyncCallback<GetEventsResult>() {
 							public void onSuccess(GetEventsResult result) {
-								for(SessionEvent e : result.getEvents()) {
-									++lastOffset;
+								for(SessionEvent<?> e : result.getEvents()) {
+									lastSequence = e.getSequence();
 									Log.info("firing event");
-									eventBus.fireEvent( getEvent(e, lastOffset));
+									GwtEvent<?> event = eventRegistry.getEvent(e);
+									if( event != null) eventBus.fireEvent( event );
 								}
 								
 								running = false;
 								Log.info("Got "+ result.getEvents().size() + " events");
 							};
 							
-							private GwtEvent<?> getEvent(SessionEvent e, int offset) {
-								if(e instanceof TransUnitUpdated) {
-									return new TransUnitUpdatedEvent( (TransUnitUpdated) e, offset);
-								}
-								throw new RuntimeException("Cannot handle event");
-							}
-
 							public void onFailure(Throwable caught) {
 								Log.info("failed");
 								running = false;
