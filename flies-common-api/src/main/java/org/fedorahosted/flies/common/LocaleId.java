77a37892956c0749649801e0eabdb91b63bf12ed
Caius Chance <cchance@redhat.com>
2009-11-24 17:28:53 +1000

    added flies-client-ant-publican

---
+++ flies-common-api/src/main/java/org/fedorahosted/flies/common/LocaleId.java
@@ -0,0 +1,54 @@
+package org.fedorahosted.flies.common;
+
+import java.io.Serializable;
+
+public final class LocaleId implements Serializable{
+
+	private static final long serialVersionUID = -7977805381672178179L;
+
+	private String id;
+	// TODO split up to language code, country code, qualifier etc..
+
+	public static final LocaleId EN = new LocaleId("en");
+	public static final LocaleId EN_US = new LocaleId("en-US");
+	
+	// JaxB needs a no-arg constructor :(
+	private LocaleId(){
+		id = null;
+	}
+	
+	public LocaleId(String localeId) {
+		if(localeId == null)
+			throw new IllegalArgumentException("localeId");
+		if(localeId.indexOf('_') != -1)
+			throw new IllegalArgumentException("expected lang[-country[-modifier]], got " + localeId);
+		this.id = localeId.intern();
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj == this) return true;
+		if (!(obj instanceof LocaleId) ) return false;
+		return this.id.equals(((LocaleId)obj).id);
+	}
+	
+	@Override
+	public int hashCode() {
+		return id.hashCode();
+	}
+
+	@Override
+	public String toString() {
+		return id;
+	}
+	
+    public static LocaleId fromJavaName(String localeName) {
+        return new LocaleId(localeName.replace('_', '-'));
+    }
+
+    public String toJavaName() {
+        return id.replace('-', '_');
+    }
+	
+	
+}
---
+++ flies-webtrans/src/main/java/org/fedorahosted/flies/webtrans/client/WestNavigationView.java
@@ -0,0 +1,42 @@
+package org.fedorahosted.flies.webtrans.client;
+
+import com.allen_sauer.gwt.log.client.Log;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HasWidgets;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+public class WestNavigationView extends FlowPanel implements
+		WestNavigationPresenter.Display {
+
+	public WestNavigationView() {
+		getElement().setId("WestNavigationView");
+	}
+
+	@Override
+	public Widget asWidget() {
+		return this;
+	}
+
+	@Override
+	public void startProcessing() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stopProcessing() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public HasWidgets getWidgets() {
+		return this;
+	}
+	
+}
