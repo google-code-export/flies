f0d7afd7b267e17c529b041c1b7316b1d080479c
Sean Flanigan <sflaniga@redhat.com>
2010-02-24 12:01:49 +1000

    fixed list of @Indexed classes

---
+++ flies-war/src/main/java/org/fedorahosted/flies/webtrans/client/TopMenuView.java
@@ -0,0 +1,135 @@
+package org.fedorahosted.flies.webtrans.client;
+
+import org.fedorahosted.flies.webtrans.editor.table.NavigationConsts;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HasText;
+import com.google.gwt.user.client.ui.HasWidgets;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Widget;
+
+public class TopMenuView extends Composite implements TopMenuPresenter.Display {
+
+	interface TopMenuUiBinder extends UiBinder<Widget, TopMenuView> {
+	}
+
+	private static TopMenuUiBinder uiBinder = GWT.create(TopMenuUiBinder.class);
+
+	@UiField
+	Label userLabel, workspaceLabel;
+
+	@UiField
+	Button nextEntryButton, prevEntryButton, nextFuzzyButton, prevFuzzyButton,
+			nextUntranslatedButton, prevUntranslatedButton;
+
+	@UiField
+	HorizontalPanel loginPanel;
+
+	public TopMenuView() {
+		initWidget(uiBinder.createAndBindUi(this));
+
+		// texts remain set by code is good for I18N
+		prevEntryButton.setText("Prev Entry");
+		nextEntryButton.setText("Next Entry");
+		prevFuzzyButton.setText("Prev Fuzzy");
+		nextFuzzyButton.setText("Next Fuzzy");
+		prevUntranslatedButton.setText("Prev Untranslated");
+		nextUntranslatedButton.setText("Next Untranslated");
+		prevEntryButton.setTitle(NavigationConsts.PREV);
+		nextEntryButton.setTitle(NavigationConsts.NEXT);
+		prevFuzzyButton.setTitle(NavigationConsts.PREV_FUZZY);
+		nextFuzzyButton.setTitle(NavigationConsts.NEXT_FUZZY);
+		prevUntranslatedButton.setTitle(NavigationConsts.PREV_NEW);
+		nextUntranslatedButton.setTitle(NavigationConsts.NEXT_NEW);
+
+		userLabel.setText("<Username>");
+		workspaceLabel.setText("Workspace");
+
+		// logoutLink = new Hyperlink("Logout", "Logout");
+
+		// transNavToolbarView = new TransNavToolbarView();
+		// rightMenu.add(transNavToolbarView);
+
+		// rightMenu.add(logoutLink);
+	}
+
+	// @Override
+	// public HasClickHandlers getLogoutLink() {
+	// return logoutLink;
+	// }
+
+	@Override
+	public HasText getProjectName() {
+		return workspaceLabel;
+	}
+
+	@Override
+	public HasText getUsername() {
+		return userLabel;
+	}
+
+	@Override
+	public HasClickHandlers getPrevEntryButton() {
+		// TODO Auto-generated method stub
+		return prevEntryButton;
+	}
+
+	@Override
+	public HasClickHandlers getNextEntryButton() {
+		// TODO Auto-generated method stub
+		return nextEntryButton;
+	}
+
+	@Override
+	public HasClickHandlers getPrevFuzzyButton() {
+		// TODO Auto-generated method stub
+		return prevFuzzyButton;
+	}
+
+	@Override
+	public HasClickHandlers getNextFuzzyButton() {
+		// TODO Auto-generated method stub
+		return nextFuzzyButton;
+	}
+
+	@Override
+	public HasClickHandlers getPrevUntranslatedButton() {
+		// TODO Auto-generated method stub
+		return prevUntranslatedButton;
+	}
+
+	@Override
+	public HasClickHandlers getNextUntranslatedButton() {
+		// TODO Auto-generated method stub
+		return nextUntranslatedButton;
+	}
+
+	@Override
+	public Widget asWidget() {
+		return this;
+	}
+
+	@Override
+	public HasWidgets getWidgets() {
+		return (HasWidgets)this.getWidget();
+	}
+
+	@Override
+	public void startProcessing() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stopProcessing() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ flies-war/src/test/java/org/fedorahosted/flies/webtrans/client/rpc/DummyDispatchAsync.java
@@ -0,0 +1,81 @@
+package org.fedorahosted.flies.webtrans.client.rpc;
+
+
+
+import net.customware.gwt.dispatch.shared.Action;
+import net.customware.gwt.dispatch.shared.Result;
+
+import org.fedorahosted.flies.gwt.rpc.ActivateWorkspaceAction;
+import org.fedorahosted.flies.gwt.rpc.ActivateWorkspaceResult;
+import org.fedorahosted.flies.gwt.rpc.EnsureLoggedInAction;
+import org.fedorahosted.flies.gwt.rpc.EnsureLoggedInResult;
+import org.fedorahosted.flies.gwt.rpc.GetDocsList;
+import org.fedorahosted.flies.gwt.rpc.GetDocsListResult;
+import org.fedorahosted.flies.gwt.rpc.GetGlossaryConcept;
+import org.fedorahosted.flies.gwt.rpc.GetGlossaryConceptResult;
+import org.fedorahosted.flies.gwt.rpc.GetProjectStatusCount;
+import org.fedorahosted.flies.gwt.rpc.GetProjectStatusCountResult;
+import org.fedorahosted.flies.gwt.rpc.GetStatusCount;
+import org.fedorahosted.flies.gwt.rpc.GetStatusCountResult;
+import org.fedorahosted.flies.gwt.rpc.GetTransUnits;
+import org.fedorahosted.flies.gwt.rpc.GetTransUnitsResult;
+import org.fedorahosted.flies.gwt.rpc.GetTranslationMemory;
+import org.fedorahosted.flies.gwt.rpc.GetTranslationMemoryResult;
+import org.fedorahosted.flies.gwt.rpc.GetTranslatorList;
+import org.fedorahosted.flies.gwt.rpc.GetTranslatorListResult;
+
+import com.allen_sauer.gwt.log.client.Log;
+import com.google.gwt.user.client.DeferredCommand;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public class DummyDispatchAsync extends SeamDispatchAsync {
+	public DummyDispatchAsync() {
+		Log.info("DummyDispatchAsync()");
+	}
+
+	@Override
+	public <A extends Action<R>, R extends Result> void execute(A action,
+			AsyncCallback<R> callback) {
+		if (action instanceof GetTransUnits) {
+			GetTransUnits gtuAction = (GetTransUnits) action;
+			AsyncCallback<GetTransUnitsResult> gtuCallback = (AsyncCallback<GetTransUnitsResult>) callback;
+			DeferredCommand.addCommand(new DummyGetTransUnitCommand(gtuAction, gtuCallback));
+		} else if (action instanceof GetDocsList) {
+			final GetDocsList gdlAction = (GetDocsList) action;
+			AsyncCallback<GetDocsListResult> gdlCallback = (AsyncCallback<GetDocsListResult>) callback;
+			DeferredCommand.addCommand(new DummyGetDocsListCommand(gdlAction, gdlCallback));
+		} else if (action instanceof ActivateWorkspaceAction) {
+			final ActivateWorkspaceAction gwcAction = (ActivateWorkspaceAction) action;
+			AsyncCallback<ActivateWorkspaceResult> gwcCallback = (AsyncCallback<ActivateWorkspaceResult>) callback;
+			DeferredCommand.addCommand(new DummyActivateWorkspaceCommand(gwcAction, gwcCallback));
+		} else if (action instanceof EnsureLoggedInAction) {
+			final EnsureLoggedInAction _action = (EnsureLoggedInAction) action;
+			AsyncCallback<EnsureLoggedInResult> _callback = (AsyncCallback<EnsureLoggedInResult>) callback;
+			DeferredCommand.addCommand(new DummyEnsureLoggedInCommand(_action, _callback));
+		} else if (action instanceof GetTranslatorList) {
+			final GetTranslatorList _action = (GetTranslatorList) action;
+			AsyncCallback<GetTranslatorListResult> _callback = (AsyncCallback<GetTranslatorListResult>) callback;
+			DeferredCommand.addCommand(new DummyGetTranslatorListCommand(_action, _callback));
+		} else if (action instanceof GetProjectStatusCount) {
+			final GetProjectStatusCount _action = (GetProjectStatusCount) action;
+			AsyncCallback<GetProjectStatusCountResult> _callback = (AsyncCallback<GetProjectStatusCountResult>) callback;
+			DeferredCommand.addCommand(new DummyGetProjectStatusCountCommand(_action, _callback));
+		} else if (action instanceof GetStatusCount) {
+			final GetStatusCount _action = (GetStatusCount) action;
+			AsyncCallback<GetStatusCountResult> _callback = (AsyncCallback<GetStatusCountResult>) callback;
+			DeferredCommand.addCommand(new DummyGetStatusCountCommand(_action, _callback));
+		} else if (action instanceof GetTranslationMemory) {
+			final GetTranslationMemory _action = (GetTranslationMemory) action;
+			AsyncCallback<GetTranslationMemoryResult> _callback = (AsyncCallback<GetTranslationMemoryResult>) callback;
+			DeferredCommand.addCommand(new DummyGetTranslationMemoryCommand(_action, _callback));
+		}  else if (action instanceof GetGlossaryConcept) {
+			final GetGlossaryConcept _action = (GetGlossaryConcept) action;
+			AsyncCallback<GetGlossaryConceptResult> _callback = (AsyncCallback<GetGlossaryConceptResult>) callback;
+			DeferredCommand.addCommand(new DummyGetGlossaryCommand(_action, _callback));
+		} else {
+			Log.warn("DummyDispatchAsync: ignoring action of "+action.getClass());
+//			callback.onFailure(new RuntimeException());
+		}
+	}
+
+}
