48e53a38c412020e13ce97971f6e7c554142d41e
Sean Flanigan <sflaniga@redhat.com>
2010-03-19 13:16:45 +1000

    Added front-end command-line client PoTool for uploading/downloading PO files

---
+++ flies-war/src/main/java/org/fedorahosted/flies/webtrans/server/GetProjectStatusCountHandler.java
@@ -0,0 +1,85 @@
+package org.fedorahosted.flies.webtrans.server;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import net.customware.gwt.dispatch.server.ActionHandler;
+import net.customware.gwt.dispatch.server.ExecutionContext;
+import net.customware.gwt.dispatch.shared.ActionException;
+
+import org.fedorahosted.flies.common.LocaleId;
+import org.fedorahosted.flies.common.TransUnitCount;
+import org.fedorahosted.flies.core.dao.DocumentDAO;
+import org.fedorahosted.flies.core.dao.ProjectContainerDAO;
+import org.fedorahosted.flies.gwt.model.DocumentId;
+import org.fedorahosted.flies.gwt.model.DocumentStatus;
+import org.fedorahosted.flies.gwt.model.ProjectContainerId;
+import org.fedorahosted.flies.gwt.rpc.GetProjectStatusCount;
+import org.fedorahosted.flies.gwt.rpc.GetProjectStatusCountResult;
+import org.fedorahosted.flies.repository.model.HDocument;
+import org.fedorahosted.flies.repository.model.HProjectContainer;
+import org.fedorahosted.flies.security.FliesIdentity;
+import org.hibernate.Session;
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Logger;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.log.Log;
+
+@Name("webtrans.gwt.GetProjectStatusCountHandler")
+@Scope(ScopeType.STATELESS)
+public class GetProjectStatusCountHandler implements ActionHandler<GetProjectStatusCount, GetProjectStatusCountResult> {
+
+		@Logger Log log;
+		
+		@In Session session;
+		
+		@In
+		ProjectContainerDAO projectContainerDAO;
+		@In
+		DocumentDAO documentDAO;
+
+		@In TranslationWorkspaceManager translationWorkspaceManager;
+		
+		@Override
+		public GetProjectStatusCountResult execute(GetProjectStatusCount action,
+				ExecutionContext context) throws ActionException {
+			
+			FliesIdentity.instance().checkLoggedIn();
+			
+			ProjectContainerId containerId = action.getProjectContainerId();
+			log.info("Fetching Doc Status List for {0}", containerId);
+			ArrayList<DocumentStatus> docliststatus = new ArrayList<DocumentStatus>(); 
+			HProjectContainer hProjectContainer = projectContainerDAO.getById(containerId.getId());
+			
+			Collection<HDocument> hDocs = hProjectContainer.getDocuments().values();
+			for (HDocument hDoc : hDocs) {
+				DocumentId docId = new DocumentId(hDoc.getId());
+								
+				TransUnitCount stat = documentDAO.getStatistics(docId.getValue(), action.getLocaleId() );
+				
+				DocumentStatus docstatus = new DocumentStatus(docId, stat);
+				docliststatus.add(docstatus);
+			}
+						
+			TranslationWorkspace workspace = translationWorkspaceManager.getWorkspace(action.getProjectContainerId().getId(), action.getLocaleId() );
+			
+			log.info("Returning Doc Status List for {0}: {1} elements", containerId, docliststatus.size());
+
+			return new GetProjectStatusCountResult(action.getProjectContainerId(), docliststatus);
+
+		}
+
+		@Override
+		public Class<GetProjectStatusCount> getActionType() {
+			// TODO Auto-generated method stub
+			return GetProjectStatusCount.class;
+		}
+
+		@Override
+		public void rollback(GetProjectStatusCount action, GetProjectStatusCountResult result,
+				ExecutionContext context) throws ActionException {
+		}
+
+}
