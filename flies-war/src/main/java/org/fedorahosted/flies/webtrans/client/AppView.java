7f6b617a1e88b266677ae5a6a43a9cf01783ddf6
Asgeir Frimannsson <asgeirf@redhat.com>
2010-03-25 15:50:09 +1000

    increase space for editor by moving documents link to the top

---
+++ flies-war/src/main/java/org/fedorahosted/flies/webtrans/client/AppView.java
@@ -0,0 +1,209 @@
+package org.fedorahosted.flies.webtrans.client;
+
+import org.fedorahosted.flies.gwt.model.DocumentInfo;
+import org.fedorahosted.flies.webtrans.client.ui.HasPager;
+import org.fedorahosted.flies.webtrans.client.ui.Pager;
+import org.fedorahosted.flies.webtrans.editor.HasTransUnitCount;
+import org.fedorahosted.flies.webtrans.editor.HasTransUnitCount.CountUnit;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.SpanElement;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.gen2.logging.shared.Log;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.DockLayoutPanel;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.LayoutPanel;
+import com.google.gwt.user.client.ui.SplitLayoutPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.inject.Inject;
+
+public class AppView extends Composite implements AppPresenter.Display {
+
+	interface AppViewUiBinder extends UiBinder<LayoutPanel, AppView> {
+	}
+
+	private static AppViewUiBinder uiBinder = GWT.create(AppViewUiBinder.class);
+
+	@UiField
+	Anchor signOutLink, leaveLink, helpLink, documentsLink;
+
+	@UiField
+	SpanElement user, selectedDocumentSpan;
+
+	@UiField
+	LayoutPanel editor;
+	
+	@UiField(provided = true)
+	final Resources resources;
+	
+	@UiField
+	DockLayoutPanel editorPanel;
+	
+	@UiField(provided=true)
+	SidePanel sidePanel;
+	
+	@UiField
+	SplitLayoutPanel mainSplitPanel;
+	
+	@UiField
+	FlowPanel tmPanel, transUnitNavigation;
+	
+	@UiField(provided=true) 
+	Pager pager;
+	
+	@UiField(provided=true)
+	TransUnitCountBar transUnitCountBar;
+	
+	private Widget documentListView;
+	private Widget editorView;
+	private Widget filterView;
+	private Widget workspaceUsersView;
+	
+	final WebTransMessages messages;
+	
+	@Inject
+	public AppView(Resources resources, WebTransMessages messages, SidePanel sidePanel) {
+		this.resources = resources;
+		this.messages = messages;
+		this.sidePanel = sidePanel;
+		this.pager = new Pager(messages, resources);
+		this.transUnitCountBar = new TransUnitCountBar(messages);
+		transUnitCountBar.setToggleEnabled(true);
+		transUnitCountBar.setCountUnit(CountUnit.TranslationUnit);
+
+		initWidget(uiBinder.createAndBindUi(this));
+		mainSplitPanel.setWidgetMinSize(sidePanel, 200);
+		mainSplitPanel.setWidgetMinSize(tmPanel, 150);
+		
+		
+		user.setInnerText("Bob the Builder");
+		pager.setVisible(false);
+		transUnitNavigation.setVisible(false);
+		
+		helpLink.setHref( messages.hrefHelpLink() );
+		helpLink.setTarget("_BLANK");
+	}
+
+	@Override
+	public Widget asWidget() {
+		return this;
+	}
+
+	@Override
+	public void startProcessing() {
+	}
+
+	@Override
+	public void stopProcessing() {
+	}
+
+	@Override
+	public void showInMainView(MainView view) {
+		switch(view) {
+		case Documents:
+			editor.setWidgetTopBottom(documentListView, 0, Unit.PX, 0, Unit.PX);
+			editor.setWidgetTopHeight(editorView, 0, Unit.PX, 0, Unit.PX);
+			pager.setVisible(false);
+			transUnitNavigation.setVisible(false);
+			break;
+		case Editor:
+			editor.setWidgetTopBottom(editorView, 0, Unit.PX, 0, Unit.PX);
+			editor.setWidgetTopHeight(documentListView, 0, Unit.PX, 0, Unit.PX);
+			pager.setVisible(true);
+			transUnitNavigation.setVisible(true);
+			break;
+		}
+	}
+
+	@Override
+	public void setDocumentListView(Widget documentListView) {
+		this.editor.add(documentListView);
+		this.documentListView = documentListView;
+	}
+	
+	@Override
+	public void setEditorView(Widget editorView) {
+		this.editor.add(editorView);
+		this.editorView = editorView;
+	}
+	
+	@Override
+	public void setTransUnitNavigationView(Widget transUnitNavigation) {
+		this.transUnitNavigation.clear();
+		this.transUnitNavigation.add(transUnitNavigation);
+	}
+	
+	@Override
+	public void setTranslationMemoryView(Widget translationMemoryView) {
+		tmPanel.clear();
+		tmPanel.add(translationMemoryView);
+	}
+	
+	@Override
+	public HasPager getTableEditorPager() {
+		return pager;
+	}
+	
+	@Override
+	public void setFilterView(Widget filterView) {
+		sidePanel.setFilterView(filterView);
+	}
+
+	@Override
+	public HasTransUnitCount getTransUnitCountBar() {
+		return transUnitCountBar;
+	}
+	
+	@Override
+	public HasClickHandlers getHelpLink() {
+		return helpLink;
+	}
+	
+	@Override
+	public HasClickHandlers getLeaveWorkspaceLink() {
+		return leaveLink;
+	}
+	
+	@Override
+	public HasClickHandlers getSignOutLink() {
+		return signOutLink;
+	}
+
+	@Override
+	public HasClickHandlers getDocumentsLink() {
+		return documentsLink;
+	}
+	
+	@Override
+	public void setTableEditorPagerVisible(boolean visible) {
+		pager.setVisible(visible);
+	}
+	
+	@Override
+	public void setTransUnitCountBarVisible(boolean visible) {
+		transUnitCountBar.setVisible(visible);
+	}
+	
+	@Override
+	public void setUserLabel(String userLabel) {
+		user.setInnerText(userLabel);
+	}
+	
+	@Override
+	public void setWorkspaceNameLabel(String workspaceNameLabel) {
+		documentsLink.setText(workspaceNameLabel);
+	}
+	
+	@Override
+	public void setSelectedDocument(DocumentInfo document) {
+		selectedDocumentSpan.setInnerText(document.getName());
+	}
+}
---
+++ flies-war/src/test/java/org/fedorahosted/flies/rest/service/ProjectsServiceSeamTest.java
@@ -0,0 +1,110 @@
+package org.fedorahosted.flies.rest.service;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.endsWith;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
+import java.net.URI;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.dbunit.operation.DatabaseOperation;
+import org.fedorahosted.flies.rest.ApiKeyHeaderDecorator;
+import org.fedorahosted.flies.rest.client.IProjectsResource;
+import org.fedorahosted.flies.rest.dto.Project;
+import org.fedorahosted.flies.rest.dto.ProjectList;
+import org.jboss.resteasy.client.ClientRequestFactory;
+import org.jboss.resteasy.client.ClientResponse;
+import org.jboss.resteasy.plugins.providers.RegisterBuiltin;
+import org.jboss.resteasy.spi.ResteasyProviderFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+@Test(groups = { "seam-tests" })
+public class ProjectsServiceSeamTest extends FliesDBUnitSeamTest {
+
+	ClientRequestFactory clientRequestFactory;
+	IProjectsResource projectService;
+
+	@BeforeClass
+	public void prepareRestEasyClientFramework() throws Exception {
+		ResteasyProviderFactory instance = ResteasyProviderFactory
+				.getInstance();
+		RegisterBuiltin.register(instance);
+
+		clientRequestFactory = new ClientRequestFactory(
+				new SeamMockClientExecutor(this), new URI("/restv1/"));
+
+		clientRequestFactory.getPrefixInterceptors().registerInterceptor(
+				new ApiKeyHeaderDecorator("admin",
+						"12345678901234567890123456789012"));
+
+		projectService = clientRequestFactory
+				.createProxy(IProjectsResource.class);
+
+	}
+
+	@Override
+	protected void prepareDBUnitOperations() {
+		beforeTestOperations.add(new DataSetOperation(
+				"org/fedorahosted/flies/test/model/ProjectData.dbunit.xml",
+				DatabaseOperation.CLEAN_INSERT));
+	}
+
+	public void retrieveListofProjects() throws Exception {
+		ClientResponse<ProjectList> response = projectService.get();
+
+		assertThat(response.getStatus(), is(200));
+		assertThat(response.getEntity(), notNullValue());
+		assertThat(response.getEntity().getProjects().size(), is(1));
+
+	}
+	
+	public void createProject(){
+		Project project = new Project("my-new-project", "My New Project", "Another test project");
+
+		projectService = clientRequestFactory.createProxy(IProjectsResource.class);
+		
+		Response response = projectService.post(project);
+		
+		assertThat( response.getStatus(), is( Status.CREATED.getStatusCode()));
+		
+		String location = (String) response.getMetadata().getFirst("Location");
+		
+		assertThat( location, endsWith("/projects/p/my-new-project"));
+		
+		ClientResponse<ProjectList> response1 = projectService.get();
+		assertThat(response1.getStatus(), is(Status.OK.getStatusCode()));
+		assertThat(response1.getEntity(), notNullValue());
+		assertThat(response1.getEntity().getProjects().size(), is(2)); 
+		Project projectRef = response1.getEntity().getProjects().get(1);
+		assertThat( projectRef.getName(), is("My New Project"));
+	}
+	
+	public void createProjectThatAlreadyExists(){
+		Project project = new Project("sample-project", "Sample Project", "An example Project");
+		Response response = projectService.post(project);
+	
+        assertThat( response.getStatus(), is( Status.CONFLICT.getStatusCode()));
+	}
+
+	public void createProjectWithInvalidData(){
+		projectService = clientRequestFactory.createProxy(IProjectsResource.class);
+
+		Project project = new Project("my,new,project", "My New Project", "Another test project");
+		Response response = projectService.post(project);
+		
+        assertThat( response.getStatus(), is( Status.BAD_REQUEST.getStatusCode()));
+        
+		projectService = clientRequestFactory.createProxy(IProjectsResource.class);
+        Project project1 = new Project("my-test-project","My test ProjectMy test ProjectMy test ProjectMy test ProjectMy test ProjectMy test Project", "Length of Project name beyond 80");
+        Response response1 = projectService.post(project1);
+        
+        assertThat(response1.getStatus(), is(Status.BAD_REQUEST.getStatusCode()));
+        
+	}
+
+}
