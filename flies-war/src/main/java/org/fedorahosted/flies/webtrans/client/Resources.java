7cfa0bd762eb2f56df9584232e6eaaefaf3771da
Sean Flanigan <sflaniga@redhat.com>
2010-03-26 16:18:31 +1000

    Added project for Selenium tests

---
+++ flies-war/src/main/java/org/fedorahosted/flies/webtrans/client/Resources.java
@@ -0,0 +1,28 @@
+package org.fedorahosted.flies.webtrans.client;
+
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
+
+/**
+ * Resources used by the entire application.
+ */
+public interface Resources extends ClientBundle {
+
+	// @Source("Style.css")
+	// Style style();
+
+	@Source("org/fedorahosted/flies/webtrans/images/flies_logo_small.png")
+	ImageResource logo();
+	
+	@Source("org/fedorahosted/flies/webtrans/images/x.png")
+	ImageResource xButton();
+	
+	@Source("org/fedorahosted/flies/webtrans/images/silk/user.png")
+	ImageResource userOnline();
+
+	@Source("org/fedorahosted/flies/webtrans/images/silk/page_white_text.png")
+	ImageResource documentImage();
+
+	@Source("org/fedorahosted/flies/webtrans/images/silk/folder.png")
+	ImageResource folderImage();
+}
