d13690a7bca98c1185f75882327577e533b2b4ca
Asgeir Frimannsson <asgeirf@redhat.com>
2010-03-26 17:22:51 +1000

    allow user to minimize TM panel

---
+++ flies-war/src/main/java/org/fedorahosted/flies/webtrans/client/WebTransMessages.java
@@ -0,0 +1,59 @@
+package org.fedorahosted.flies.webtrans.client;
+
+import com.google.gwt.i18n.client.Messages;
+import com.google.gwt.i18n.client.LocalizableResource.DefaultLocale;
+import com.google.gwt.i18n.client.LocalizableResource.Generate;
+import com.google.gwt.i18n.client.Messages.DefaultMessage;
+
+@DefaultLocale("en_US")
+@Generate(format = "com.google.gwt.i18n.rebind.format.PropertiesFormat")
+public interface WebTransMessages extends Messages {
+	
+	@DefaultMessage("{0} users online")
+	@PluralText({"one", "One user online"})
+	@Description("Title of the minimized users panel")
+	String nUsersOnline(@PluralCount int numUsers);
+
+	
+	@DefaultMessage("{0}%")
+	String statusBarLabelPercentage(int approved, @Optional int needReview, @Optional int untranslated);
+	
+	@DefaultMessage("{0}/{1}/{2}")
+	String statusBarLabelUnits(int approved, int needReview, int untranslated);
+	
+	@DefaultMessage("http://code.google.com/docreader/#p=flies&s=flies&t=TranslatorGuide")
+	String hrefHelpLink();
+
+
+	@DefaultMessage("{0} to {1} - Flies Web Translation")
+	String windowTitle(String workspaceName, String localeName);
+	
+	@DefaultMessage("first Page")
+	String firstPage();
+
+	@DefaultMessage("Home")
+	String firstPageShortcut();
+
+	@DefaultMessage("previous Page")	
+	String prevPage();
+	
+	@DefaultMessage("PageUp")
+	String prevPageShortcut();
+	
+	@DefaultMessage("next Page")
+	String nextPage();
+	
+	@DefaultMessage("PageDown")			
+	String nextPageShortcut();
+	
+	@DefaultMessage("last Page")
+	String lastPage();
+	
+	@DefaultMessage("End")
+	String lastPageShortcut();
+
+
+	@DefaultMessage("Show Translation Suggestions")
+	String showTranslationMemoryPanel();
+	
+}
