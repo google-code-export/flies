b08bf9298823dac813ff6757ea4d1478ea85f8f4
Sean Flanigan <sflaniga@redhat.com>
2010-03-29 14:18:03 +1000

    Told prettify to use XML highlighting

---
+++ flies-war/src/main/java/org/fedorahosted/flies/webtrans/client/ui/Pager.java
@@ -0,0 +1,178 @@
+package org.fedorahosted.flies.webtrans.client.ui;
+import org.fedorahosted.flies.webtrans.client.Resources;
+import org.fedorahosted.flies.webtrans.client.WebTransMessages;
+import org.fedorahosted.flies.webtrans.editor.table.NavigationMessages;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextBox;
+
+public class Pager extends Composite implements HasPager {
+
+	interface PagerUiBinder extends UiBinder<HTMLPanel, Pager> {
+	}
+
+	private static PagerUiBinder uiBinder = GWT.create(PagerUiBinder.class);
+	
+	@UiField
+	Anchor firstPage, lastPage, nextPage, previousPage;
+
+	@UiField
+	Label firstPageDisabled, lastPageDisabled, nextPageDisabled, previousPageDisabled;
+	
+	@UiField
+	TextBox gotoPage;
+	
+	@UiField
+	Label pageCountLabel;
+	
+	@UiField(provided=true) 
+	Resources resources;
+	
+	private int pageCount = PAGECOUNT_UNKNOWN;
+	private int currentPage;
+	
+	public static final int PAGECOUNT_UNKNOWN = -1;
+	
+	private final WebTransMessages messages;
+	
+	public Pager(final WebTransMessages messages, final Resources resources) {
+		this.messages = messages;
+		this.resources = resources;
+		initWidget(uiBinder.createAndBindUi(this));
+		firstPageDisabled.setVisible(false);
+		lastPageDisabled.setVisible(false);
+		nextPageDisabled.setVisible(false);
+		previousPageDisabled.setVisible(false);
+
+		firstPage.setText( messages.firstPage() );
+
+		firstPage.setTitle( messages.firstPageShortcut() );
+		firstPageDisabled.setText( messages.firstPage() );
+		
+		previousPage.setText( messages.prevPage() );
+		previousPage.setTitle( messages.prevPageShortcut() );
+		previousPageDisabled.setText( messages.prevPage() );
+		
+		nextPage.setText( messages.nextPage() );
+		nextPage.setTitle( messages.nextPageShortcut() );
+		nextPageDisabled.setText( messages.nextPage() );
+		
+		lastPage.setText( messages.lastPage() );
+		lastPage.setTitle( messages.lastPageShortcut() );
+		lastPageDisabled.setText( messages.lastPage() );
+	}
+
+	@Override
+	protected void onLoad() {
+		super.onLoad();
+		gotoPage.addKeyUpHandler(new KeyUpHandler() {
+			
+			@Override
+			public void onKeyUp(KeyUpEvent event) {
+				if( event.getNativeKeyCode() == KeyCodes.KEY_ENTER ) {
+					try{
+						int newValue = Integer.parseInt( gotoPage.getText() );
+						setValue(newValue);
+					}
+					catch(NumberFormatException nfe){}
+				}
+			}
+		});
+
+		firstPage.addClickHandler(clickHandler);
+		lastPage.addClickHandler(clickHandler);
+		previousPage.addClickHandler(clickHandler);
+		nextPage.addClickHandler(clickHandler);
+		refresh();
+	}
+
+	private void setEnabled(Anchor link, Label diabledLabel, boolean enabled) {
+		link.setVisible(enabled);
+		diabledLabel.setVisible(!enabled);
+	}
+	
+	private void refresh(){
+		String page = pageCount == PAGECOUNT_UNKNOWN ? "" : "of " + pageCount;
+		pageCountLabel.setText(page);
+		setEnabled(firstPage, firstPageDisabled, currentPage != 1);
+		setEnabled(previousPage, previousPageDisabled, currentPage != 1);
+
+		setEnabled(nextPage, nextPageDisabled, currentPage != pageCount );
+		setEnabled(lastPage, lastPageDisabled, currentPage != pageCount && pageCount != PAGECOUNT_UNKNOWN);
+
+		gotoPage.setText( String.valueOf(currentPage) );
+	}
+	
+	@Override
+	public void setPageCount(int pageCount) {
+		this.pageCount = pageCount;
+		refresh();
+	}
+	
+	@Override
+	public int getPageCount() {
+		return pageCount;
+	}
+
+	@Override
+	public Integer getValue() {
+		return currentPage;
+	}
+
+	@Override
+	public void setValue(Integer value) {
+		setValue(value, true);
+	}
+
+	@Override
+	public void setValue(Integer value, boolean fireEvents) {
+		if(value != this.currentPage){
+			this.currentPage = value;
+			ValueChangeEvent.fire(this, value);
+			if(fireEvents) {
+				ValueChangeEvent.fire(this, value);
+			}
+			refresh();
+		}
+	}
+
+	@Override
+	public HandlerRegistration addValueChangeHandler(
+			ValueChangeHandler<Integer> handler) {
+		return addHandler(handler, ValueChangeEvent.getType());
+	}
+
+	private final ClickHandler clickHandler = new ClickHandler() {
+		
+		@Override
+		public void onClick(ClickEvent event) {
+			if(event.getSource() == firstPage) {
+				setValue(1);
+			}
+			else if(event.getSource() == lastPage) {
+				setValue(pageCount);
+			} 
+			else if(event.getSource() == nextPage) {
+				setValue(currentPage +1);
+			} 
+			else if(event.getSource() == previousPage) {
+				setValue(currentPage -1);
+			} 
+		}
+	};
+
+}
---
+++ flies-war/src/main/java/org/fedorahosted/flies/webtrans/server/rpc/ActivateWorkspaceHandler.java
@@ -0,0 +1,89 @@
+package org.fedorahosted.flies.webtrans.server.rpc;
+
+import java.util.Collections;
+import java.util.HashSet;
+
+import net.customware.gwt.dispatch.server.ActionHandler;
+import net.customware.gwt.dispatch.server.ExecutionContext;
+import net.customware.gwt.dispatch.shared.ActionException;
+
+import org.fedorahosted.flies.core.model.HPerson;
+import org.fedorahosted.flies.gwt.auth.Identity;
+import org.fedorahosted.flies.gwt.auth.Permission;
+import org.fedorahosted.flies.gwt.auth.Role;
+import org.fedorahosted.flies.gwt.auth.SessionId;
+import org.fedorahosted.flies.gwt.common.WorkspaceContext;
+import org.fedorahosted.flies.gwt.model.Person;
+import org.fedorahosted.flies.gwt.model.PersonId;
+import org.fedorahosted.flies.gwt.rpc.ActivateWorkspaceAction;
+import org.fedorahosted.flies.gwt.rpc.ActivateWorkspaceResult;
+import org.fedorahosted.flies.gwt.rpc.EnterWorkspace;
+import org.fedorahosted.flies.repository.model.HProjectContainer;
+import org.fedorahosted.flies.security.FliesIdentity;
+import org.fedorahosted.flies.webtrans.server.ActionHandlerFor;
+import org.fedorahosted.flies.webtrans.server.TranslationWorkspace;
+import org.fedorahosted.flies.webtrans.server.TranslationWorkspaceManager;
+import org.hibernate.Session;
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Logger;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.contexts.Contexts;
+import org.jboss.seam.log.Log;
+import org.jboss.seam.web.ServletContexts;
+
+@Name("webtrans.gwt.ActivateWorkspaceHandler")
+@Scope(ScopeType.STATELESS)
+@ActionHandlerFor(ActivateWorkspaceAction.class)
+public class ActivateWorkspaceHandler extends AbstractActionHandler<ActivateWorkspaceAction, ActivateWorkspaceResult> {
+
+	@Logger Log log;
+	
+	@In Session session;
+	
+	@In TranslationWorkspaceManager translationWorkspaceManager;
+
+	@Override
+	public ActivateWorkspaceResult execute(ActivateWorkspaceAction action, ExecutionContext context)
+			throws ActionException {
+		
+		FliesIdentity.instance().checkLoggedIn();
+
+		TranslationWorkspace workspace = translationWorkspaceManager.getOrRegisterWorkspace(action.getWorkspaceId());
+		
+		workspace.registerTranslator(ActivateWorkspaceHandler.retrieveSessionId(), ActivateWorkspaceHandler.retrievePersonId());
+		
+		HProjectContainer hProjectContainer = (HProjectContainer) session.get(HProjectContainer.class, action.getWorkspaceId().getProjectContainerId().getId());
+		
+		//Send EnterWorkspace event to clients
+		EnterWorkspace event = new EnterWorkspace(new PersonId(FliesIdentity.instance().getPrincipal().getName()));
+		workspace.publish(event);
+
+		Identity identity = new Identity( 
+				retrieveSessionId(), 
+				retrievePerson(), new HashSet<Permission>(), new HashSet<Role>());
+		
+		return new ActivateWorkspaceResult(workspace.getWorkspaceContext(), identity);
+	}
+
+	@Override
+	public void rollback(ActivateWorkspaceAction action, ActivateWorkspaceResult result,
+			ExecutionContext context) throws ActionException {
+	}
+
+	public static SessionId retrieveSessionId() {
+		return new SessionId(ServletContexts.instance().getRequest().getSession().getId());
+	}
+
+	public static PersonId retrievePersonId(){
+		HPerson authenticatedPerson = (HPerson) Contexts.getSessionContext().get("authenticatedPerson");
+		return new PersonId(authenticatedPerson.getAccount().getUsername());
+	}
+
+	public static Person retrievePerson(){
+		HPerson authenticatedPerson = (HPerson) Contexts.getSessionContext().get("authenticatedPerson");
+		return new Person( new PersonId(authenticatedPerson.getAccount().getUsername()), authenticatedPerson.getName());
+	}
+	
+}
