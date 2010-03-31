2c4d9c6d907376c83f0ab24ad05b7bf38f113a41
Sean Flanigan <sflaniga@redhat.com>
2010-03-31 12:07:22 +1000

    Reverted 13a6b1313b until I can fix the rpm generation

---
+++ flies-gwt-api/src/main/java/org/fedorahosted/flies/gwt/rpc/GetProjectStatusCount.java
@@ -0,0 +1,13 @@
+package org.fedorahosted.flies.gwt.rpc;
+
+import org.fedorahosted.flies.common.LocaleId;
+import org.fedorahosted.flies.gwt.common.WorkspaceId;
+import org.fedorahosted.flies.gwt.model.DocumentId;
+import org.fedorahosted.flies.gwt.model.ProjectContainerId;
+
+public class GetProjectStatusCount extends AbstractWorkspaceAction<GetProjectStatusCountResult>{
+	
+	public GetProjectStatusCount() {
+	}
+
+}
---
+++ flies-war/src/main/java/org/fedorahosted/flies/webtrans/editor/filter/TransFilterView.java
@@ -0,0 +1,84 @@
+package org.fedorahosted.flies.webtrans.editor.filter;
+
+import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.DisclosurePanel;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.Widget;
+
+public class TransFilterView extends Composite implements TransFilterPresenter.Display {
+	private static final boolean INITIALLY_OPEN = true; // for now, anyway
+
+	private FlowPanel bodyPanel;
+	private Button findButton = new Button("Find");
+	private TextBox replaceText = new TextBox();
+	private Button replaceButton = new Button("Replace");
+	private Button replaceAllButton = new Button("Replace All");
+	private Widget filterUnitPanel;
+	
+	public TransFilterView() {
+		bodyPanel = new FlowPanel();
+//		bodyPanel.setWidth("100%");
+
+		Panel filterButtonBar = new FlowPanel();
+		filterButtonBar.setStyleName("float-right-div");
+		filterButtonBar.add(findButton);
+		bodyPanel.add(filterButtonBar);
+
+		bodyPanel.add(replaceText);
+		
+		Panel replaceButtonBar = new FlowPanel();
+		replaceButtonBar.setStyleName("float-right-div");
+		replaceButtonBar.add(replaceButton);
+		replaceButtonBar.add(replaceAllButton);
+		bodyPanel.add(replaceButtonBar);
+		
+//		RoundedContainerWithHeader container = new RoundedContainerWithHeader(new Label("Find Messages"), bodyPanel);
+//		initWidget(container);
+		
+		DisclosurePanel container = new DisclosurePanel("Find Messages");
+		container.setOpen(INITIALLY_OPEN);
+		container.setAnimationEnabled(false);
+		
+		container.add(bodyPanel);
+		initWidget(container);
+
+		getElement().setId("TransFilterView");
+	}
+	
+	@Override
+	public void setFilterUnitPanel (Widget widget) {
+		if (filterUnitPanel != null) {
+			bodyPanel.remove(filterUnitPanel);
+			filterUnitPanel = null;
+		} else {
+			bodyPanel.insert(widget,0);
+			filterUnitPanel = widget;
+		}
+	}
+
+	@Override
+	public Widget asWidget() {
+		return this;
+	}
+
+	@Override
+	public void startProcessing() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stopProcessing() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	@Override
+	public HasClickHandlers getFindButton() {
+		return findButton;
+	}
+}
