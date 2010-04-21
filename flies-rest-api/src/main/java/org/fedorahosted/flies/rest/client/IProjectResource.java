f2df070dacad7332cb5c379d6e68a8c4a22e3d44
Sean Flanigan <sflaniga@redhat.com>
2010-04-21 11:25:39 +1000

    Now touches web.xml during exploded deployment

---
+++ flies-rest-api/src/main/java/org/fedorahosted/flies/rest/client/IProjectResource.java
@@ -0,0 +1,28 @@
+package org.fedorahosted.flies.rest.client;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.fedorahosted.flies.rest.MediaTypes;
+import org.fedorahosted.flies.rest.dto.Project;
+import org.jboss.resteasy.client.ClientResponse;
+
+//@Path("/projects/p/{projectSlug}")
+public interface IProjectResource {
+	
+	@GET
+	@Produces({ MediaTypes.APPLICATION_FLIES_PROJECT_XML, MediaTypes.APPLICATION_FLIES_PROJECT_ITERATION_JSON })
+	public ClientResponse<Project> get();
+
+	@PUT
+	@Consumes({ MediaTypes.APPLICATION_FLIES_PROJECT_XML, MediaTypes.APPLICATION_FLIES_PROJECT_ITERATION_JSON })
+	public Response put(Project project);
+
+}
---
+++ flies-war/src/test/java/org/fedorahosted/flies/webtrans/server/rpc/GetTransMemoryHandlerTest.java
@@ -0,0 +1,34 @@
+package org.fedorahosted.flies.webtrans.server.rpc;
+
+import org.testng.annotations.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.fedorahosted.flies.webtrans.server.rpc.GetTransMemoryHandler.toFuzzyLuceneQuery;
+import static org.fedorahosted.flies.webtrans.server.rpc.GetTransMemoryHandler.toLuceneQuery;
+
+@Test(groups={"unit-tests"})
+public class GetTransMemoryHandlerTest {
+
+	// list of special chars taken from
+	// http://lucene.apache.org/java/2_4_1/queryparsersyntax.html#Escaping%20Special%20Characters
+
+	public void testLuceneQuery(){
+		assertThat(
+			toLuceneQuery("plaintext"), 
+			is("plaintext"));
+		assertThat(
+			toLuceneQuery("lucene special characters + - && || ! ( ) " +
+					"{ } [ ] ^ \" ~ * ? : \\ plus % _"), 
+			is("lucene special characters \\+ \\- \\&\\& \\|\\| \\! \\( \\) " +
+					"\\{ \\} \\[ \\] \\^ \\\" \\~ \\* \\? \\: \\\\ plus % _"));
+	}
+	
+	public void testFuzzyLuceneQuery(){
+		assertThat(
+				toFuzzyLuceneQuery(" lucene special characters + - && || ! ( ) " +
+				"{ } [ ] ^ \" ~ * ? : \\ plus % _ "), 
+				is(" lucene~ special~ characters~ \\+~ \\-~ \\&\\&~ \\|\\|~ \\!~ \\(~ \\)~ " +
+				"\\{~ \\}~ \\[~ \\]~ \\^~ \\\"~ \\~~ \\*~ \\?~ \\:~ \\\\~ plus~ %~ _~ "));
+	}
+}
