993f9a051226d13e4f1171a6e2665aa6f02aa051
Ding-Yi Chen <dchen@redhat.com>
2010-05-12 14:33:50 +1000

    Add cmake targets for createproj createiter and upload

---
+++ flies-rest-api/src/main/java/org/fedorahosted/flies/rest/dto/v1/SourceAsTargetResource.java
@@ -0,0 +1,19 @@
+package org.fedorahosted.flies.rest.dto.v1;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+import org.fedorahosted.flies.common.Namespaces;
+
+@XmlType(name="sourceAsTargetResourceType", namespace=Namespaces.FLIES, propOrder={})
+@XmlRootElement(name="resource",namespace=Namespaces.FLIES)
+public class SourceAsTargetResource extends AbstractContentTranslationResource<SourceAsTargetTextFlow> {
+
+	public SourceAsTargetResource() {
+	}
+
+	public SourceAsTargetResource(String id, String name) {
+		super(id, name);
+	}
+	
+}
