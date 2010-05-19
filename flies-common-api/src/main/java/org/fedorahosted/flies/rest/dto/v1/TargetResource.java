e92dff55ec7a6c6ab265c1cbb99f7071f8624583
Sean Flanigan <sflaniga@redhat.com>
2010-05-19 17:53:38 +1000

    1. To ensure correct eclipse configuration, eclipse:{clean,eclipse} are now disabled unless eclipse profile is activated. Example: mvn -Peclipse eclipse:clean eclipse:eclipse
    2. Eclipse now compiles to */bin, not */target/classes, to avoid using
    the JBoss4 persistence.xml in Eclipse, or vice versa.

---
+++ flies-common-api/src/main/java/org/fedorahosted/flies/rest/dto/v1/TargetResource.java
@@ -0,0 +1,19 @@
+package org.fedorahosted.flies.rest.dto.v1;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+import org.fedorahosted.flies.common.Namespaces;
+
+@XmlType(name="targetResourceType", namespace=Namespaces.FLIES, propOrder={})
+@XmlRootElement(name="resource",namespace=Namespaces.FLIES)
+public class TargetResource extends AbstractContentTranslationResource<MultiTargetTextFlow> {
+
+	public TargetResource() {
+	}
+
+	public TargetResource(String id, String name) {
+		super(id, name);
+	}
+	
+}
