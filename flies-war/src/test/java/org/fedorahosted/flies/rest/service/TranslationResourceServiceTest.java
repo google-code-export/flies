a4b73d4db6d8b5f6f1a158174915c1fc691d7f33
Asgeir Frimannsson <asgeirf@redhat.com>
2010-05-25 11:27:24 +1000

    ensure only seam tests are run in the integration-test phase

---
+++ flies-war/src/test/java/org/fedorahosted/flies/rest/service/TranslationResourceServiceTest.java
@@ -0,0 +1,48 @@
+package org.fedorahosted.flies.rest.service;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import javax.ws.rs.core.Response.Status;
+
+import org.dbunit.operation.DatabaseOperation;
+import org.fedorahosted.flies.FliesRestTest;
+import org.fedorahosted.flies.dao.DocumentDAO;
+import org.fedorahosted.flies.dao.ProjectIterationDAO;
+import org.fedorahosted.flies.rest.client.ITranslationResources;
+import org.fedorahosted.flies.rest.dto.v1.ResourcesList;
+import org.jboss.resteasy.client.ClientResponse;
+import org.testng.annotations.Test;
+
+public class TranslationResourceServiceTest extends FliesRestTest {
+
+	@Override
+	protected void prepareDBUnitOperations() {
+		beforeTestOperations.add(new DataSetOperation(
+				"META-INF/testdata/ProjectsData.dbunit.xml",
+				DatabaseOperation.CLEAN_INSERT));
+	}
+
+	@Override
+	protected void prepareResources() {
+		final ProjectIterationDAO projectIterationDAO = new ProjectIterationDAO(getSession());
+		final DocumentDAO documentDAO = new DocumentDAO(getSession());
+		final DocumentUtils documentUtils = new DocumentUtils();
+		
+		TranslationResourcesService obj = new TranslationResourcesService(
+				projectIterationDAO, documentDAO, documentUtils);
+		
+		resources.add(obj);
+	}
+	
+	@Test
+	public void fetchListOfResources() {
+		
+		ITranslationResources client = getClientRequestFactory().createProxy(ITranslationResources.class, "/projects/p/sample-project/iterations/i/1.0/resources");
+		ClientResponse<ResourcesList> resources = client.get();
+		assertThat(resources.getResponseStatus(), is(Status.OK));
+		assertThat(resources.getEntity().size(), is(0));
+
+	}
+
+}
