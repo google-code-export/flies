d9dbd99c6741d0e9fc4ef748e5e4fad3d6909b7c
Asgeir Frimannsson <asgeirf@redhat.com>
2010-06-03 12:13:25 +1000

    ensure all DAOs are Stateless Seam components

---
+++ flies-war/src/main/java/org/fedorahosted/flies/security/FliesIdentity.java
@@ -0,0 +1,81 @@
+package org.fedorahosted.flies.security;
+
+import static org.jboss.seam.ScopeType.SESSION;
+import static org.jboss.seam.annotations.Install.APPLICATION;
+
+import javax.security.auth.Subject;
+import javax.security.auth.login.LoginContext;
+import javax.security.auth.login.LoginException;
+
+import org.fedorahosted.flies.dao.AccountDAO;
+import org.fedorahosted.flies.model.HAccount;
+import org.jboss.resteasy.plugins.server.embedded.SimplePrincipal;
+import org.jboss.seam.Component;
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Install;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.annotations.Startup;
+import org.jboss.seam.annotations.intercept.BypassInterceptors;
+import org.jboss.seam.contexts.Contexts;
+import org.jboss.seam.core.Events;
+import org.jboss.seam.log.LogProvider;
+import org.jboss.seam.log.Logging;
+import org.jboss.seam.security.Identity;
+import org.jboss.seam.security.NotLoggedInException;
+
+@Name("org.jboss.seam.security.identity")
+@Scope(SESSION)
+@Install(precedence = APPLICATION)
+@BypassInterceptors
+@Startup
+public class FliesIdentity extends Identity {
+	
+	public static final String USER_LOGOUT_EVENT = "user.logout";
+	public static final String USER_ENTER_WORKSPACE = "user.enter";
+	private String username;
+
+	private static final LogProvider log = Logging
+			.getLogProvider(FliesIdentity.class);
+
+	private String apiKey;
+
+	public String getApiKey() {
+		return apiKey;
+	}
+
+	public void setApiKey(String apiKey) {
+		this.apiKey = apiKey;
+		getCredentials().setPassword(apiKey);
+	}
+
+	public boolean isApiRequest() {
+		return apiKey != null;
+	}
+
+	public static FliesIdentity instance() {
+		if (!Contexts.isSessionContextActive()) {
+			throw new IllegalStateException("No active session context");
+		}
+
+		FliesIdentity instance = (FliesIdentity) Component.getInstance(
+				FliesIdentity.class, ScopeType.SESSION);
+
+		if (instance == null) {
+			throw new IllegalStateException("No Identity could be created");
+		}
+
+		return instance;
+	}
+	
+	public void checkLoggedIn(){
+		if(!isLoggedIn())
+			throw new NotLoggedInException();
+	}
+	
+	public void logout() {
+		if (Events.exists()) Events.instance().raiseEvent(USER_LOGOUT_EVENT, getPrincipal().getName());
+		super.logout();
+	}
+	
+}
