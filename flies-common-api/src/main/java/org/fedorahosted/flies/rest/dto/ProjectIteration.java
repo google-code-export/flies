3cec5efc8432273e26ab670c3470e0a2ba609912
Asgeir Frimannsson <asgeirf@redhat.com>
2010-06-03 13:03:05 +1000

    avoid logging exception when clients send bad content

---
+++ flies-common-api/src/main/java/org/fedorahosted/flies/rest/dto/ProjectIteration.java
@@ -0,0 +1,31 @@
+package org.fedorahosted.flies.rest.dto;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSeeAlso;
+import javax.xml.bind.annotation.XmlType;
+
+import org.fedorahosted.flies.common.Namespaces;
+
+
+@XmlType(name="projectIterationType", namespace=Namespaces.FLIES, propOrder={})
+@XmlRootElement(name="project-iteration", namespace=Namespaces.FLIES)
+public class ProjectIteration extends AbstractProjectIteration {
+
+	public ProjectIteration() {
+	}
+	
+	public ProjectIteration(ProjectIteration other) {
+		super(other);
+	}
+
+	public ProjectIteration(ProjectIterationRes other) {
+		super(other);
+	}
+	
+	public ProjectIteration(String id, String name, String description) {
+		super(id, name, description);
+	}
+
+}
