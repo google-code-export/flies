6103fcba323c240d6cfcea227438b0b692721a29
Asgeir Frimannsson <asgeirf@redhat.com>
2010-06-07 13:24:30 +1000

    issue #72 - Fix to generate flies url automatically in GWT client

---
+++ flies-war/src/main/java/org/fedorahosted/flies/webtrans/client/Application.java
@@ -0,0 +1,182 @@
+package org.fedorahosted.flies.webtrans.client;
+
+import net.customware.gwt.presenter.client.place.PlaceManager;
+import net.customware.gwt.presenter.client.place.PlaceRequestEvent;
+
+import org.fedorahosted.flies.common.LocaleId;
+import org.fedorahosted.flies.webtrans.client.EventProcessor.StartCallback;
+import org.fedorahosted.flies.webtrans.client.gin.WebTransGinjector;
+import org.fedorahosted.flies.webtrans.shared.auth.Identity;
+import org.fedorahosted.flies.webtrans.shared.model.ProjectIterationId;
+import org.fedorahosted.flies.webtrans.shared.model.WorkspaceContext;
+import org.fedorahosted.flies.webtrans.shared.model.WorkspaceId;
+import org.fedorahosted.flies.webtrans.shared.rpc.ActivateWorkspaceAction;
+import org.fedorahosted.flies.webtrans.shared.rpc.ActivateWorkspaceResult;
+import org.fedorahosted.flies.webtrans.shared.rpc.ExitWorkspaceAction;
+import org.fedorahosted.flies.webtrans.shared.rpc.ExitWorkspaceResult;
+
+import com.allen_sauer.gwt.log.client.Log;
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.logical.shared.CloseEvent;
+import com.google.gwt.event.logical.shared.CloseHandler;
+import com.google.gwt.http.client.URL;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.Window.Location;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Application implements EntryPoint{
+
+	private static String fliesUrl = null;
+	private static WorkspaceId workspaceId;
+	private static WorkspaceContext workspaceContext;
+	private static Identity identity;
+	
+	private final WebTransGinjector injector = GWT.create(WebTransGinjector.class);
+	public void onModuleLoad() {
+		
+		injector.getDispatcher().execute(
+			new ActivateWorkspaceAction( getWorkspaceId() ), new AsyncCallback<ActivateWorkspaceResult>() {
+
+				@Override
+				public void onFailure(Throwable caught) {
+					redirectToLogin();
+				}
+
+				@Override
+				public void onSuccess(ActivateWorkspaceResult result) {
+					workspaceContext = result.getWorkspaceContext();
+					identity = result.getIdentity();
+					injector.getDispatcher().setIdentity(identity);
+					injector.getDispatcher().setWorkspaceContext(workspaceContext);
+					startApp();
+				}
+				
+			}
+		);
+	}
+	
+	private void startApp() {
+		
+		// When user close the workspace, send ExitWorkSpaceAction
+		Window.addCloseHandler(new CloseHandler<Window>() {
+			@Override
+			public void onClose(CloseEvent<Window> event) {
+//				injector.getDispatcher().execute(new ExitWorkspaceAction(),
+//						new AsyncCallback<ExitWorkspaceResult>() {
+//							@Override
+//							public void onFailure(Throwable caught) {
+//
+//							}
+//
+//							@Override
+//							public void onSuccess(ExitWorkspaceResult result) {
+//							}
+//
+//						});
+			}
+		});
+		
+		
+		final EventProcessor eventProcessor = injector.getEventProcessor();
+		eventProcessor.start( new StartCallback() {
+			@Override
+			public void onSuccess() {
+				delayedStartApp();
+			}
+			
+			@Override
+			public void onFailure(Throwable e) {
+				RootLayoutPanel.get().add( 
+						new HTML("<h1>Failed to start Event Service...</h1>" 
+						+ "<b>Exception:</b> "+ e.getMessage()));
+			}
+		});
+
+	}
+	
+	private void delayedStartApp() {
+		final AppPresenter appPresenter = injector.getAppPresenter();
+		RootLayoutPanel.get().add( appPresenter.getDisplay().asWidget() );
+		appPresenter.bind();
+
+        // Needed because of this bug:
+        // http://code.google.com/p/gwt-presenter/issues/detail?id=6
+        PlaceManager placeManager = injector.getPlaceManager();
+        injector.getEventBus().addHandler( PlaceRequestEvent.getType(), placeManager );
+
+		injector.getPlaceManager().fireCurrentPlace();
+	}
+	
+	public static ProjectIterationId getProjectIterationId() {
+		String projectSlug = Window.Location.getParameter("project");
+		String iterationSlug = Window.Location.getParameter("iteration");
+		if(projectSlug == null || iterationSlug == null)
+			return null;
+		try{
+			return new ProjectIterationId(projectSlug, iterationSlug);
+		}
+		catch(NumberFormatException nfe){
+			return null;
+		}
+	}
+
+	public static LocaleId getLocaleId() {
+		String localeId = Window.Location.getParameter("localeId");
+		return localeId == null ? null : new LocaleId(localeId);
+	}
+
+	public static void redirectToLogin() {
+		redirectToUrl( getFliesUrl() + "account/sign_in?continue=" + URL.encodeComponent(Window.Location.getHref()));	
+	}
+	
+	public static void redirectToLogout() {
+		redirectToUrl( getFliesUrl() + "account/sign_out");	
+	}
+
+	public static void redirectToFliesProjectHome(WorkspaceId workspaceId) {
+		redirectToUrl( getFliesUrl() + "project/view/"+ workspaceId.getProjectIterationId().getProjectSlug());	
+	}
+	
+	public static native void redirectToUrl(String url)/*-{
+		$wnd.location = url;
+	}-*/;
+	
+	public static native void closeWindow()/*-{
+		$wnd.close();
+	}-*/;
+
+	public static WorkspaceId getWorkspaceId() {
+		if(workspaceId == null) {
+			// TODO handle null values
+			workspaceId = new WorkspaceId(getProjectIterationId(), getLocaleId());
+		}
+		return workspaceId;
+	}
+	
+	public static WorkspaceContext getWorkspaceContext() {
+		return workspaceContext;
+	}
+	
+	public static Identity getIdentity() {
+		return identity;
+	}
+	
+	public static String getFliesUrl() {
+		if(fliesUrl == null) {
+			StringBuilder str = new StringBuilder(Window.Location.getProtocol());
+			str.append("//");
+			str.append(Window.Location.getHost());
+			str.append( PathUtils.getContextRoot( Window.Location.getPath() ) );
+			fliesUrl = str.toString();
+		}
+		return fliesUrl;
+	}
+	
+}
---
+++ flies-war/src/main/java/org/fedorahosted/flies/webtrans/client/rpc/SeamDispatchAsync.java
@@ -0,0 +1,87 @@
+package org.fedorahosted.flies.webtrans.client.rpc;
+
+import net.customware.gwt.dispatch.shared.Action;
+
+import net.customware.gwt.dispatch.shared.Result;
+
+import org.fedorahosted.flies.webtrans.client.Application;
+import org.fedorahosted.flies.webtrans.shared.DispatchService;
+import org.fedorahosted.flies.webtrans.shared.DispatchServiceAsync;
+import org.fedorahosted.flies.webtrans.shared.auth.AuthenticationError;
+import org.fedorahosted.flies.webtrans.shared.auth.AuthorizationError;
+import org.fedorahosted.flies.webtrans.shared.auth.Identity;
+import org.fedorahosted.flies.webtrans.shared.model.WorkspaceContext;
+import org.fedorahosted.flies.webtrans.shared.rpc.AbstractWorkspaceAction;
+
+import com.allen_sauer.gwt.log.client.Log;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.rpc.ServiceDefTarget;
+import com.google.inject.Inject;
+
+public class SeamDispatchAsync implements CachingDispatchAsync {
+
+	private static final DispatchServiceAsync realService;
+	
+	static{
+		realService = GWT.create(DispatchService.class);
+	}
+	
+	protected WorkspaceContext workspaceContext;
+	protected Identity identity;
+	
+	@Inject
+	public SeamDispatchAsync() {
+		final String endpointURL = Application.getFliesUrl() + "seam/resource/gwt";
+
+		((ServiceDefTarget) realService).setServiceEntryPoint(endpointURL);
+	}
+	
+	public <A extends Action<R>, R extends Result> void execute(final A action,
+			final AsyncCallback<R> callback) {
+
+		if( action instanceof AbstractWorkspaceAction<?> ) {
+			AbstractWorkspaceAction<?> wsAction = (AbstractWorkspaceAction<?>) action;
+			if(workspaceContext == null || identity == null) {
+				callback.onFailure( new AuthorizationError("Dispatcher not set up to delegate WorkspaceContext and Identity") );
+				return;
+			}
+			wsAction.setSessionId(identity.getSessionId());
+			wsAction.setWorkspaceId(workspaceContext.getWorkspaceId());
+		}
+		
+		realService.execute(action, new AsyncCallback<Result>() {
+
+			public void onFailure(final Throwable caught) {
+				if(caught instanceof AuthenticationError) {
+					Application.redirectToLogin();
+				}
+				else if(caught instanceof AuthorizationError){
+					Log.info("RCP Authorization Error calling " + action.getClass() + ": " + caught.getMessage());
+					callback.onFailure(caught);
+				} 
+				else {
+					callback.onFailure(caught);
+				}
+			}
+
+			@SuppressWarnings("unchecked")
+			public void onSuccess(final Result result) {
+				callback.onSuccess((R) result);
+			}
+		});
+	}
+	
+	@Override
+	public void setWorkspaceContext(WorkspaceContext workspaceContext) {
+		this.workspaceContext = workspaceContext;
+	}
+	
+	@Override
+	public void setIdentity(Identity identity) {
+		this.identity = identity;
+	}
+	
+	
+	
+}
